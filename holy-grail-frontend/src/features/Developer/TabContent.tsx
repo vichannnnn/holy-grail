a835a5c4750e7c8f75315173a58936a8e82b7c09
Sebastian Ong <sebastian.ong@hotmail.com>
2023-06-15 18:01:33 +0800

    feat: add search and pagination to dev page

--- holy-grail-frontend/src/features/Developer/TabContent.tsx
+++ holy-grail-frontend/src/features/Developer/TabContent.tsx
@@ -12,9 +12,12 @@ import {
   TableContainer,
   TableHead,
   TableRow,
+  OutlinedInput,
+  TablePagination,
 } from "@mui/material";
 import EditIcon from "@mui/icons-material/Edit";
 import React from "react";
+import { Tab } from "@chakra-ui/react";
 
 type DataTypeKey = "categories" | "subjects" | "types";
 
@@ -33,23 +36,80 @@ export const TabContent = ({
   handleAdd,
   type,
 }: TabContentProps) => {
+  const [query, setQuery] = React.useState<string>('');
+  const [page, setPage] = React.useState<number>(0);
+  const [chunkSize, setChunkSize] = React.useState<number>(10);
+
   const handleEditClick = (id: number) => {
     handleEdit(id, type);
   };
+
+  //filter data based on query
+  const handleValidData = () => {
+    let validData: Array<CategoryType | SubjectType | DocumentType> = data.filter((option) => {
+      return option.name.toLowerCase().includes(query.toLowerCase());
+    });
+    return validData;
+  }
+
+  //paginate data into chunks
+  const handlePaging = () =>{
+    let pagedData: Array<Array<CategoryType | SubjectType | DocumentType>> = [];
+    let validData: Array<CategoryType | SubjectType | DocumentType> = handleValidData();
+    for (let i = 0; i < validData.length; i += chunkSize ) {
+      pagedData.push(
+        validData.slice(i, i + chunkSize)
+      );
+    };
+    console.log(pagedData);
+    return pagedData;
+  }
+  
+
+
+  
+  
 
   return (
     <TableContainer component={Paper}>
       <Table>
         <TableHead>
           <TableRow>
-            <TableCell>{title}</TableCell>
+    
+            <TableCell>
+              <OutlinedInput
+                sx={{ width: '120%' }}
+                placeholder={`Search for ${title}`}
+                onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
+                  setQuery(event.target.value);
+                  handleValidData();
+                  
+                }}
+              />
+              
+            </TableCell>
+            
             <TableCell align="right">
-              <Button onClick={handleAdd}>+</Button>
+                <Button onClick={handleAdd}>+ {title}</Button>
             </TableCell>
           </TableRow>
+          <TablePagination 
+            count={handleValidData().length}
+            onPageChange={(event: React.MouseEvent<HTMLButtonElement> | null, page: number) =>{
+              setPage(page);
+            }}
+            page={page}
+            rowsPerPage={chunkSize}
+            rowsPerPageOptions={[5,10,15,20]}
+            onRowsPerPageChange={(event: React.ChangeEvent<HTMLInputElement>) => {
+              setChunkSize(parseInt(event.target.value));
+              handlePaging();
+            }}
+          /> 
         </TableHead>
         <TableBody>
-          {data.map((item) => (
+
+          {(handlePaging()[page] || []).map((item) => (
             <TableRow key={item.id}>
               <TableCell component="th" scope="row">
                 {item.name}
@@ -65,7 +125,10 @@ export const TabContent = ({
             </TableRow>
           ))}
         </TableBody>
+        
       </Table>
+      
     </TableContainer>
+    
   );
 };
