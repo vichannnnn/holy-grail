5b655ac0556e168f3af5a9373718541005075301
SebassNoob <78428559+SebassNoob@users.noreply.github.com>
2023-06-16 23:58:58 +0800

    Feat/admin edit note (#14)
    * feat: ability to edit notes as admin
    
    * fix: admin edit note shows up blank
    
    * fix: race condition after submission
    
    * fix: update the type for extras and comboboxprops
    
    * style: yarn format
    
    * fix: resolve code review comments
    
    ---------
    
    Co-authored-by: hima7459 <violet@himaa.me>

--- holy-grail-frontend/src/features/Library/Combobox.tsx
+++ holy-grail-frontend/src/features/Library/Combobox.tsx
@@ -3,12 +3,16 @@ import TextField from "@mui/material/TextField";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 import { CSSProperties } from "react";
 
+export interface Extras {
+  disablePortal?: boolean;
+}
 export interface ComboboxProps {
   label: string;
   value: number | "";
   onChange: (newValue: number | "") => void;
   options: { value: number; label: string }[];
   style?: CSSProperties;
+  extras?: Extras;
 }
 
 const Combobox = ({
@@ -17,6 +21,7 @@ const Combobox = ({
   onChange,
   options,
   style,
+  extras,
 }: ComboboxProps) => {
   const muiTheme = createTheme();
   return (
@@ -29,6 +34,7 @@ const Combobox = ({
         renderInput={(params) => <TextField {...params} label={label} />}
         isOptionEqualToValue={(option, value) => option.value === value?.value}
         getOptionLabel={(option) => option.label}
+        {...extras}
       />
     </ThemeProvider>
   );
