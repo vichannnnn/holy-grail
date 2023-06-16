cf14ada4931ba3f95461d52c71055e9de6f1aa11
hima7459 <violet@himaa.me>
2023-06-16 22:56:02 +0800

    fix: update the type for extras and comboboxprops

--- holy-grail-frontend/src/features/Library/Combobox.tsx
+++ holy-grail-frontend/src/features/Library/Combobox.tsx
@@ -3,13 +3,16 @@ import TextField from "@mui/material/TextField";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 import { CSSProperties } from "react";
 
+export interface Extras {
+  disablePortal?: boolean
+}
 export interface ComboboxProps {
   label: string;
   value: number | "";
   onChange: (newValue: number | "") => void;
   options: { value: number; label: string }[];
   style?: CSSProperties;
-  extras?: any; // extra properties to give to the combobox
+  extras?: Extras;
 }
 
 const Combobox = ({
@@ -27,8 +30,7 @@ const Combobox = ({
         style={style}
         value={options.find((option) => option.value === value) || null}
         options={options}
-        onChange={(_, newValue: any) => onChange(newValue?.value || "")}
-        // please help: i dont think i broke anything but why does typehinting newValue as any work?
+        onChange={(_, newValue) => onChange(newValue?.value || "")}
         renderInput={(params) => <TextField {...params} label={label} />}
         isOptionEqualToValue={(option, value) => option.value === value?.value}
         getOptionLabel={(option) => option.label}
