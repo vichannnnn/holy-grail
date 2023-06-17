45d2b205db482d13698edda2717d1b32a7bd27d4
Sebastian Ong <sebastian.ong@hotmail.com>
2023-06-17 17:07:03 +0800

    fix: fix pr comments

--- holy-grail-frontend/src/components/Pagination/Pagination.tsx
+++ holy-grail-frontend/src/components/Pagination/Pagination.tsx
@@ -1,24 +1,28 @@
 import { Button, HStack, Text } from "@chakra-ui/react";
 import debounce from "lodash/debounce";
 
+interface HStackStyles {
+  spacing?: number;
+  mt?: string;
+}
 interface PaginationProps {
   pageInfo: { page: number; size: number; total: number; pages: number };
   handlePageChange: (newPage: number) => void;
-  styles?: { mt: string };
+  HStackStyles?: HStackStyles;
 }
 
 export const Pagination = ({
   pageInfo,
   handlePageChange,
-  styles,
+  HStackStyles,
 }: PaginationProps) => {
   const debouncedHandlePageChange = debounce(handlePageChange, 100);
 
   return (
     <HStack
-      spacing={4}
+      spacing={HStackStyles ? HStackStyles?.spacing : 4}
       justifyContent="center"
-      mt={styles?.mt ? styles.mt : "10%"}
+      mt={HStackStyles ? HStackStyles?.mt : "10%"}
     >
       <Button
         onClick={() => debouncedHandlePageChange(pageInfo.page - 1)}
