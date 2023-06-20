ba66f508ca7a5c2329ddcc4390f340cb772689b6
Darryl Ng <51407026+ddaarrrryyll@users.noreply.github.com>
2023-06-20 21:22:22 +0800

    rebase

--- holy-grail-frontend/src/features/Developer/TabContent.tsx
+++ holy-grail-frontend/src/features/Developer/TabContent.tsx
@@ -12,6 +12,7 @@ import {
   TableContainer,
   TableHead,
   TableRow,
+  TablePagination,
   OutlinedInput,
 } from "@mui/material";
 import EditIcon from "@mui/icons-material/Edit";
@@ -30,12 +31,12 @@ interface TabContentProps {
 }
 
 export const TabContent = ({
-  title,
-  data,
-  handleEdit,
-  handleAdd,
-  type,
-}: TabContentProps) => {
+                             title,
+                             data,
+                             handleEdit,
+                             handleAdd,
+                             type,
+                           }: TabContentProps) => {
   const [query, setQuery] = useState<string>("");
   const [page, setPage] = useState<number>(0);
   const [chunkSize, setChunkSize] = useState<number>(10);
@@ -46,16 +47,16 @@ export const TabContent = ({
 
   const handleFilterContent = () => {
     let validData: Array<CategoryType | SubjectType | DocumentType> =
-      data.filter((option) => {
-        return option.name.toLowerCase().includes(query.toLowerCase());
-      });
+        data.filter((option) => {
+          return option.name.toLowerCase().includes(query.toLowerCase());
+        });
     return validData;
   };
 
   const handlePaging = () => {
     let pagedData: Array<Array<CategoryType | SubjectType | DocumentType>> = [];
     let validData: Array<CategoryType | SubjectType | DocumentType> =
-      handleFilterContent();
+        handleFilterContent();
     for (let i = 0; i < validData.length; i += chunkSize) {
       pagedData.push(validData.slice(i, i + chunkSize));
     }
@@ -63,64 +64,80 @@ export const TabContent = ({
   };
 
   return (
-    <TableContainer component={Paper}>
-      <Table>
-        <TableHead>
-          <TableRow>
-            <TableCell>
-              <OutlinedInput
-                sx={{ width: "120%" }}
-                placeholder={`Search for ${title}`}
-                onChange={(event: ChangeEvent<HTMLInputElement>) => {
-                  setQuery(event.target.value);
-                  //go back to first page to prevent overflow
-                  setPage(0);
-                  handleFilterContent();
-                }}
-              />
-            </TableCell>
-
-            <TableCell align="right">
-              <Button onClick={handleAdd}>+ {title}</Button>
-            </TableCell>
-          </TableRow>
-          <TableRow>
-            <TableCell colSpan={2}>
-              <ChakraProvider>
-                <Pagination
-                  pageInfo={{
-                    page: page + 1,
-                    size: chunkSize,
-                    total: handleFilterContent().length,
-                    pages: handlePaging().length,
-                  }}
-                  handlePageChange={(newPage: number) => {
-                    setPage(newPage - 1);
-                  }}
-                  styles={{ mt: "0%" }}
+      <TableContainer component={Paper}>
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell>
+                <OutlinedInput
+                    sx={{ width: "120%" }}
+                    placeholder={`Search for ${title}`}
+                    onChange={(event: ChangeEvent<HTMLInputElement>) => {
+                      setQuery(event.target.value);
+                      //go back to first page to prevent overflow
+                      setPage(0);
+                      handleFilterContent();
+                    }}
                 />
-              </ChakraProvider>
-            </TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {(handlePaging()[page] || []).map((item) => (
-            <TableRow key={item.id}>
-              <TableCell component="th" scope="row">
-                {item.name}
               </TableCell>
+
               <TableCell align="right">
-                <Button onClick={() => handleEditClick(item.id)}>
-                  <EditIcon />
-                </Button>
-                {/*<Button onClick={() => handleDelete(item.id)}>*/}
-                {/*  <DeleteIcon />*/}
-                {/*</Button>*/}
+                <Button onClick={handleAdd}>+ {title}</Button>
               </TableCell>
             </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
+            <TableRow>
+              <TableCell colSpan={2}>
+                <ChakraProvider>
+                  <Pagination
+                      pageInfo={{
+                        page: page + 1,
+                        size: chunkSize,
+                        total: handleFilterContent().length,
+                        pages: handlePaging().length,
+                      }}
+                      handlePageChange={(newPage: number) => {
+                        setPage(newPage - 1);
+                      }}
+                      styles={{ mt: "0%" }}
+                  />
+                </ChakraProvider>
+                <Button onClick={handleAdd}>+ {title}</Button>
+              </TableCell>
+            </TableRow>
+            <TablePagination
+                count={handleFilterContent().length}
+                onPageChange={(event: React.MouseEvent<HTMLButtonElement> | null, page: number) =>{
+                  setPage(page);
+                }}
+                page={page}
+                rowsPerPage={chunkSize}
+                rowsPerPageOptions={[5,10,15,20]}
+                onRowsPerPageChange={(event: React.ChangeEvent<HTMLInputElement>) => {
+                  setChunkSize(parseInt(event.target.value));
+                  //go back to first page to prevent overflow
+                  setPage(0);
+                  handlePaging();
+                }}
+            />
+          </TableHead>
+          <TableBody>
+            {(handlePaging()[page] || []).map((item) => (
+                <TableRow key={item.id}>
+                  <TableCell component="th" scope="row">
+                    {item.name}
+                  </TableCell>
+                  <TableCell align="right">
+                    <Button onClick={() => handleEditClick(item.id)}>
+                      <EditIcon />
+                    </Button>
+                    {/*<Button onClick={() => handleDelete(item.id)}>*/}
+                    {/*  <DeleteIcon />*/}
+                    {/*</Button>*/}
+                  </TableCell>
+                </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
   );
 };
