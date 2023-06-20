3e56fe26625fa52124a82ad40ac16d3a69d6c9e5
Sebastian Ong <sebastian.ong@hotmail.com>
2023-06-20 15:17:32 +0800

    fix: do approvalscreen

--- holy-grail-frontend/src/features/Approval/ApprovalScreen.tsx
+++ holy-grail-frontend/src/features/Approval/ApprovalScreen.tsx
@@ -1,17 +1,17 @@
-import { Flex } from "@chakra-ui/react";
+import { Box } from "@mui/material";
 import { Text } from "../../components/Text/Text";
 import { Title } from "../../components/Title/Title";
 
 const ApprovalScreen = () => {
   return (
     <div id="upload">
-      <Flex direction="column" bgColor="white">
+      <Box sx={{ display: "flex", flexDirection: "column" }}>
         <Title>Administrator Panel</Title>
         <Text fontSize="xl" mt="3%" mb="10%">
           This is a list of unapproved notes and where you can choose to approve
           or delete the submitted notes.
         </Text>
-      </Flex>
+      </Box>
     </div>
   );
 };
