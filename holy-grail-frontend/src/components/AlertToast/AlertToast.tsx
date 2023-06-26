67004e0214478b6418eea0bee643ebc2f889cf49
SebassNoob <sebastian.ong@hotmail.com>
2023-06-26 18:02:05 +0800

    feat: add alertToast in mui replacing chakra toast

---
+++ holy-grail-frontend/src/components/AlertToast/AlertToast.tsx
@@ -0,0 +1,41 @@
+import {
+  Snackbar,
+  Alert,
+  ThemeProvider,
+  createTheme,
+  Typography,
+} from '@mui/material';
+
+interface AlertProps {
+  title: string;
+  description: string;
+  severity: 'success' | 'error' | 'warning' | 'info';
+}
+
+interface AlertToastProps {
+  openAlert: boolean;
+  onClose: () => void;
+  alert: AlertProps;
+}
+
+const AlertToast = ({ openAlert, onClose, alert }: AlertToastProps) => {
+  const muiTheme = createTheme();
+  return (
+    <ThemeProvider theme={muiTheme}>
+      <Snackbar
+        open={openAlert}
+        autoHideDuration={3000}
+        onClose={onClose}
+        anchorOrigin={{ vertical: 'bottom', horizontal: 'center' }}
+      >
+        <Alert severity={alert.severity} sx={{ width: '100%' }}>
+          <Typography fontWeight='bold'>{alert.title}</Typography>
+          <Typography>{alert.description}</Typography>
+        </Alert>
+      </Snackbar>
+    </ThemeProvider>
+  );
+};
+
+export default AlertToast;
+export type { AlertProps };
--- holy-grail-frontend/src/features/AccountVerify/AccountVerifyPage.tsx
+++ holy-grail-frontend/src/features/AccountVerify/AccountVerifyPage.tsx
@@ -4,13 +4,15 @@ import { verifyAccount } from '../../api/utils/auth/VerifyAccount';
 import { Link, useToast } from '@chakra-ui/react';
 import { resendVerificationEmail } from '../../api/utils/auth/ResendVerificationEmail';
 import '../SignIn/login.css';
+import AlertToast, {AlertProps} from '../../components/AlertToast/AlertToast';
 
 const VerifyAccountPage = () => {
   const [token, setToken] = useState<string | null>(null);
   const [resetStatus, setResetStatus] = useState<string | null>(null);
   const [isFailed, setFailed] = useState<boolean>(false);
+  const [alert, setAlert] = useState<AlertProps>({} as AlertProps);
+  const [openAlert, setOpenAlert] = useState<boolean>(false);
   const location = useLocation();
-  const toast = useToast();
 
   const parseQuery = (query: string): URLSearchParams => {
     return new URLSearchParams(query);
@@ -39,22 +41,21 @@ const VerifyAccountPage = () => {
   const handleResendVerificationEmail = async () => {
     try {
       await resendVerificationEmail();
-      toast({
+      setAlert({
         title: 'Verification email resent successfully.',
-        description: 'Please check your email for the verification mail sent to you.',
-        status: 'success',
-        duration: 3000,
-        isClosable: true,
+        description:
+          'Please check your email for the verification mail sent to you.',
+        severity: 'success',
       });
+      setOpenAlert(true);
     } catch (error) {
       console.error(error);
-      toast({
+      setAlert({
         title: 'Failed to resend verification email.',
         description: 'An error occurred while sending.',
-        status: 'error',
-        duration: 3000,
-        isClosable: true,
+        severity: 'error',
       });
+      setOpenAlert(true);
     }
   };
 
@@ -74,6 +75,11 @@ const VerifyAccountPage = () => {
             to send another verification email.
           </div>
         ) : null}
+        <AlertToast
+          openAlert={openAlert}
+          onClose={() => setOpenAlert(false)}
+          alert={alert}
+        />
       </div>
     </section>
   );
