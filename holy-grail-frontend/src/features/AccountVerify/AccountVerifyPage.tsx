62e6faaa720875cbaa018f95ffc03a660d33248b
SebassNoob <sebastian.ong@hotmail.com>
2023-06-27 17:43:24 +0800

    fix: remove unused imports

--- holy-grail-frontend/src/features/AccountVerify/AccountVerifyPage.tsx
+++ holy-grail-frontend/src/features/AccountVerify/AccountVerifyPage.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useLocation } from 'react-router-dom';
 import { verifyAccount } from '../../api/utils/auth/VerifyAccount';
 import { Link } from '@chakra-ui/react';
