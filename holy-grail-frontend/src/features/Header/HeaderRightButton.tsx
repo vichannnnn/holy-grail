5225f12537b6dfa59cf03e798f7c8ee14964a5be
SebassNoob <sebastian.ong@hotmail.com>
2023-07-02 22:03:26 +0800

    fix: restore header right button old behaviour

--- holy-grail-frontend/src/features/Header/HeaderRightButton.tsx
+++ holy-grail-frontend/src/features/Header/HeaderRightButton.tsx
@@ -1,56 +1,88 @@
 import { IconButton, Menu, MenuItem, Button, Typography } from '@mui/material';
-import { useState } from 'react';
+import { useEffect, useState, useContext } from 'react';
 import DensityMediumIcon from '@mui/icons-material/DensityMedium';
 import { ThemeProvider, createTheme } from '@mui/material/styles';
+import MediaQueryContext from '../../providers/MediaQueryProvider';
+import AuthContext from '../../providers/AuthProvider';
+import { useNavigate } from 'react-router-dom';
 
 interface HeaderRightButtonProps {
   children: { label: string; callback: () => void }[];
 }
 
+const DesktopButtonFace = ({
+  label,
+  onClick,
+}: {
+  label: string;
+  onClick: (event: React.MouseEvent<HTMLButtonElement>) => void;
+}) => {
+  return (
+    <Button onClick={onClick} disableRipple={true} variant='outlined' sx={{ borderColor: 'black' }}>
+      <Typography sx={{ textTransform: 'capitalize', color: 'black' }}>{label}</Typography>
+    </Button>
+  );
+};
+const MobileButtonFace = ({
+  onClick,
+}: {
+  onClick: (event: React.MouseEvent<HTMLButtonElement>) => void;
+}) => {
+  return (
+    <IconButton
+      onClick={onClick}
+      aria-label='Menu'
+      sx={{ borderRadius: '10%' }}
+      disableRipple={true}
+    >
+      <DensityMediumIcon />
+    </IconButton>
+  );
+};
+
 export const HeaderRightButton = ({ children }: HeaderRightButtonProps) => {
   const muiTheme = createTheme();
   const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+  const { isDesktop } = useContext(MediaQueryContext);
+  const { user } = useContext(AuthContext);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    setAnchorEl(null);
+  }, [isDesktop, user]);
 
   return (
     <ThemeProvider theme={muiTheme}>
-      {children.length === 1 ? (
-        <Button onClick={children[0].callback} disableRipple={true}>
-          <Typography sx={{ textTransform: 'capitalize', color: 'black' }}>
-            {children[0].label}
-          </Typography>
-        </Button>
+      {isDesktop ? (
+        user ? (
+          <DesktopButtonFace
+            label={user.username}
+            onClick={(event) => setAnchorEl(event.currentTarget)}
+          />
+        ) : (
+          <DesktopButtonFace label={'Log In'} onClick={(event) => navigate('/login')} />
+        )
       ) : (
-        <>
-          <IconButton
-            onClick={(event: React.MouseEvent<HTMLButtonElement>) =>
-              setAnchorEl(event.currentTarget)
-            }
-            aria-label='Menu'
-            sx={{ borderRadius: '10%' }}
-            disableRipple={true}
-          >
-            <DensityMediumIcon />
-          </IconButton>
-          <Menu
-            open={anchorEl !== null}
-            anchorEl={anchorEl}
-            onClose={() => setAnchorEl(null)}
-            disableScrollLock={true}
-            anchorOrigin={{
-              vertical: 'bottom',
-              horizontal: 'right',
-            }}
-            transformOrigin={{
-              vertical: 'top',
-              horizontal: 'right',
-            }}
-          >
-            {children.map((child) => (
-              <MenuItem onClick={child.callback}>{child.label}</MenuItem>
-            ))}
-          </Menu>
-        </>
+        <MobileButtonFace onClick={(event) => setAnchorEl(event.currentTarget)} />
       )}
+      <Menu
+        open={anchorEl !== null}
+        anchorEl={anchorEl}
+        onClose={() => setAnchorEl(null)}
+        disableScrollLock={true}
+        anchorOrigin={{
+          vertical: 'bottom',
+          horizontal: 'right',
+        }}
+        transformOrigin={{
+          vertical: 'top',
+          horizontal: 'right',
+        }}
+      >
+        {children.map((child) => (
+          <MenuItem onClick={child.callback}>{child.label}</MenuItem>
+        ))}
+      </Menu>
     </ThemeProvider>
   );
 };
