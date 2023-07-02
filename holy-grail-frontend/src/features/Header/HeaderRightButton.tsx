bfb6fdd97b41de94679eb062f45aefd18a00aebd
SebassNoob <78428559+SebassNoob@users.noreply.github.com>
2023-07-02 20:24:37 +0800

    feat: rewrite menu dropdown in mui (#28)

--- holy-grail-frontend/src/features/Header/HeaderRightButton.tsx
+++ holy-grail-frontend/src/features/Header/HeaderRightButton.tsx
@@ -1,27 +1,58 @@
-import { Button, Text } from '@chakra-ui/react';
-import { ButtonHTMLAttributes, forwardRef } from 'react';
+import { IconButton, Menu, MenuItem, Button, Typography } from '@mui/material';
+import { useState } from 'react';
+import DensityMediumIcon from '@mui/icons-material/DensityMedium';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
 
-export const HeaderRightButton = forwardRef<
-  HTMLButtonElement,
-  ButtonHTMLAttributes<HTMLButtonElement>
->(({ children, ...props }, ref) => {
+interface HeaderRightButtonProps {
+  children: { label: string; callback: () => void }[];
+}
+
+export const HeaderRightButton = ({ children }: HeaderRightButtonProps) => {
+  const muiTheme = createTheme();
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+
   return (
-    <>
-      <Button
-        ref={ref}
-        variant='outline'
-        borderColor='black'
-        bg='white'
-        px='30'
-        h='30px'
-        {...props}
-      >
-        <Text fontWeight='semibold' color='black'>
-          {children}
-        </Text>
-      </Button>
-    </>
+    <ThemeProvider theme={muiTheme}>
+      {children.length === 1 ? (
+        <Button onClick={children[0].callback} disableRipple={true}>
+          <Typography sx={{ textTransform: 'capitalize', color: 'black' }}>
+            {children[0].label}
+          </Typography>
+        </Button>
+      ) : (
+        <>
+          <IconButton
+            onClick={(event: React.MouseEvent<HTMLButtonElement>) =>
+              setAnchorEl(event.currentTarget)
+            }
+            aria-label='Menu'
+            sx={{ borderRadius: '10%' }}
+            disableRipple={true}
+          >
+            <DensityMediumIcon />
+          </IconButton>
+          <Menu
+            open={anchorEl !== null}
+            anchorEl={anchorEl}
+            onClose={() => setAnchorEl(null)}
+            disableScrollLock={true}
+            anchorOrigin={{
+              vertical: 'bottom',
+              horizontal: 'right',
+            }}
+            transformOrigin={{
+              vertical: 'top',
+              horizontal: 'right',
+            }}
+          >
+            {children.map((child) => (
+              <MenuItem onClick={child.callback}>{child.label}</MenuItem>
+            ))}
+          </Menu>
+        </>
+      )}
+    </ThemeProvider>
   );
-});
+};
 
 HeaderRightButton.displayName = 'HeaderRightButton';
