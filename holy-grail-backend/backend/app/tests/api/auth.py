256258c4e7b77b583677b4491bd5f93176404197
SebassNoob <78428559+SebassNoob@users.noreply.github.com>
2023-07-07 22:55:58 +0800

    Feat/update tests (#42)

--- holy-grail-backend/backend/app/tests/api/auth.py
+++ holy-grail-backend/backend/app/tests/api/auth.py
@@ -9,11 +9,11 @@ GET_USER_URL = "/auth/get"
 LOGIN_URL = "/auth/login"
 
 
-def test_create_valid_user(
-    test_client: TestClient, test_valid_user: schemas.auth.AccountSchema
+def test_create_valid_user_and_login(
+    test_not_logged_in_client: TestClient, test_valid_user: schemas.auth.AccountSchema
 ):
     payload = jsonable_encoder(test_valid_user)
-    response = test_client.post(CREATE_URL, json=payload)
+    response = test_not_logged_in_client.post(CREATE_URL, json=payload)
     resp_data = response.json()
     assert response.status_code == status.HTTP_200_OK
     assert resp_data["username"] == test_valid_user.username
@@ -24,11 +24,7 @@ def test_create_valid_user(
         "user_id": 1,
         "email": test_valid_user.email,
     }
-
-
-def test_login(test_client: TestClient, test_valid_user: schemas.auth.AccountSchema):
-    payload = jsonable_encoder(test_valid_user)
-    response = test_client.post(LOGIN_URL, json=payload)
+    response = test_not_logged_in_client.post(LOGIN_URL, json=payload)
 
     assert response.status_code == status.HTTP_200_OK
     assert response.json()["data"]["username"] == test_valid_user.username
