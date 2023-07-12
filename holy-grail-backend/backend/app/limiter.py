2e23310b8f38f1b81d2e43e3f743f11645d046ad
hima <violet@himaa.me>
2023-07-12 20:49:40 +0800

    chore(rate-limit): update ipaddr rate limit logic

--- holy-grail-backend/backend/app/limiter.py
+++ holy-grail-backend/backend/app/limiter.py
@@ -1,8 +1,20 @@
 from slowapi import Limiter
-from slowapi.util import get_remote_address
+from starlette.requests import Request
 import os
 
-limiter = Limiter(key_func=get_remote_address)
+
+def get_ipaddr(request: Request) -> str:
+    if "X-Forwarded-For" in request.headers:
+        x_forwarded_for = request.headers["X-Forwarded-For"]
+        ip_addresses = [ip.strip() for ip in x_forwarded_for.split(",")]
+        return ip_addresses[-1]
+    else:
+        if not request.client or not request.client.host:
+            return "127.0.0.1"
+        return request.client.host
+
+
+limiter = Limiter(key_func=get_ipaddr)
 
 
 def conditional_rate_limit(*args, **kwargs):
