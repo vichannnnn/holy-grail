e8d6c9c219207752deb3159af0976e48edc13b0c
vi.channnnn <54580948+vichannnnn@users.noreply.github.com>
2023-07-17 02:39:07 +0800

    chore(account-verify): update account verify page and header (#69)

--- holy-grail-frontend/vite.config.ts
+++ holy-grail-frontend/vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import path from 'path';
+import * as path from 'path';
 
 export default ({ mode }) => {
   return defineConfig({
