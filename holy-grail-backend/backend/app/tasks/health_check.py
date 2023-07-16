9787f7f606967c2340f8f75e35fa8160d7f5cdb4
hima <violet@himaa.me>
2023-07-16 17:43:10 +0800

    fix: fix ping task url

--- holy-grail-backend/backend/app/tasks/health_check.py
+++ holy-grail-backend/backend/app/tasks/health_check.py
@@ -5,5 +5,5 @@ from app.utils.worker import celery_app
 
 @celery_app.task(name="ping")
 def ping() -> None:
-    resp = requests.get("http://holy-grail-backend:8000/hello")
+    resp = requests.get("http://holy-grail-backend:8005/hello")
     return resp.json()
