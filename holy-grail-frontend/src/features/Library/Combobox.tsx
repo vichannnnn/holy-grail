e1719dcb4b5727b825b7bef4835b04a0b357f53e
hima <violet@himaa.me>
2023-07-16 20:59:34 +0800

    feat(combobox): add keyword filter on frontend

--- holy-grail-frontend/src/features/Library/Combobox.tsx
+++ holy-grail-frontend/src/features/Library/Combobox.tsx
@@ -1,25 +1,33 @@
 import {
   Autocomplete,
   AutocompleteProps,
-  TextField,
   createTheme,
+  TextField,
   ThemeProvider,
 } from '@mui/material';
-
-export interface Element {
-  value: number;
-  label: string;
-}
+import { SyntheticEvent } from 'react';
+import { CommonType } from '@api/library';
 
 export interface ComboboxProps
   extends Omit<
-    AutocompleteProps<Element, false, false, boolean>,
-    'onChange' | 'value' | 'onInputChange' | 'options' | 'renderInput'
+    AutocompleteProps<CommonType, false, false, boolean>,
+    | 'onChange'
+    | 'value'
+    | 'onInputChange'
+    | 'options'
+    | 'renderInput'
+    | 'isOptionEqualToValue'
+    | 'renderOption'
+    | 'renderTags'
+    | 'filterOptions'
+    | 'getOptionDisabled'
+    | 'groupBy'
+    | 'onHighlightChange'
   > {
   label: string;
   value: number | '';
   onChange: (newValue: number | '') => void;
-  options: Element[];
+  options: CommonType[];
 }
 
 export const Combobox = ({ label, value, onChange, options, ...props }: ComboboxProps) => {
@@ -28,12 +36,22 @@ export const Combobox = ({ label, value, onChange, options, ...props }: Combobox
   return (
     <ThemeProvider theme={muiTheme}>
       <Autocomplete
-        value={options.find((option) => option.value === value) || null}
+        value={options.find((option) => option.id === value) || null}
         options={options}
-        onChange={(_, newValue: Element) => onChange(newValue?.value || '')}
+        onChange={(event: SyntheticEvent<Element, Event>, newValue: string | CommonType | null) => {
+          if (typeof newValue === 'string' || newValue === null) {
+            onChange('');
+          } else {
+            onChange(newValue.id);
+          }
+        }}
         renderInput={(params) => <TextField {...params} label={label} />}
-        isOptionEqualToValue={(option, value: Element) => option.value === value?.value}
-        getOptionLabel={(option: Element) => option.label}
+        isOptionEqualToValue={(option: string | CommonType, value: string | CommonType) =>
+          typeof option !== 'string' && typeof value !== 'string' ? option.id === value.id : false
+        }
+        getOptionLabel={(option: CommonType | string) =>
+          typeof option === 'string' ? option : option.name
+        }
         {...props}
       />
     </ThemeProvider>
