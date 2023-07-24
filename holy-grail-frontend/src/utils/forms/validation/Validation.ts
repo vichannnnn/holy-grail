018abb8745fe71372134f481fa9dde3d42c8b36a
SebassNoob <sebastian.ong@hotmail.com>
2023-07-24 09:43:29 +0800

    chore: move account validation into shared file

--- holy-grail-frontend/src/utils/forms/validation/Validation.ts
+++ holy-grail-frontend/src/utils/forms/validation/Validation.ts
@@ -40,3 +40,24 @@ export const UpdatePasswordValidation = Yup.object().shape({
     .oneOf([Yup.ref('password'), undefined], 'Passwords must match')
     .required('Repeat password is required'),
 });
+
+export const ChangeEmailValidation = Yup.object().shape({
+  email: Yup.string().email('Invalid email').required('Email is required'),
+});
+
+export const ChangePasswordValidation = Yup.object().shape({
+  currentPassword: Yup.string().required('Password is required'),
+
+  newPassword: Yup.string()
+    .matches(/[!@#$%^&*]/, 'Password must contain a special character.')
+    .matches(/[A-Z]/, 'Password must contain an uppercase letter.')
+    .min(8, 'Password must be at least 8 characters long.')
+    .max(30, 'Password cannot exceed 30 characters.')
+    .required('Password is required'),
+
+  repeatPassword: Yup.string()
+    .test('repeatPassword-log', 'Repeat password does not match new password', (value, context) => {
+      return value === context.parent.newPassword;
+    })
+    .required('Repeat password is required'),
+});
--- holy-grail-frontend/src/utils/forms/validation/index.ts
+++ holy-grail-frontend/src/utils/forms/validation/index.ts
@@ -1,2 +1,1 @@
-export * from './SignUpValidation';
-export * from './AccountUpdateValidation';
+export * from './Validation';
