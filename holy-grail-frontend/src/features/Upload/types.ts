7ea919630ecc23458b0694afde98956e8655014f
hima <violet@himaa.me>
2023-08-14 17:55:18 +0800

    update types

--- holy-grail-frontend/src/features/Upload/types.ts
+++ holy-grail-frontend/src/features/Upload/types.ts
@@ -34,3 +34,23 @@ export interface FileSelectProps {
 export interface SelectedFilesProps {
   [key: string]: [File, string];
 }
+
+// Upload error types
+
+export enum UploadNoteErrorType {
+  DOCUMENT_NAME_DUPLICATED = 'DOCUMENT_NAME_DUPLICATED',
+  DOCUMENT_NAME_IN_DB = 'DOCUMENT_NAME_IN_DB',
+  SCHEMA_VALIDATION_ERROR = 'SCHEMA_VALIDATION_ERROR',
+  INVALID_FILE_TYPE = 'INVALID_FILE_TYPE',
+}
+
+export const UploadNoteErrorText: Record<UploadNoteErrorType, string> = {
+  [UploadNoteErrorType.DOCUMENT_NAME_DUPLICATED]:
+    'You are uploading multiple documents with the same name.',
+  [UploadNoteErrorType.DOCUMENT_NAME_IN_DB]: 'Document name already exists.',
+  [UploadNoteErrorType.SCHEMA_VALIDATION_ERROR]:
+    'Please ensure your document name is between 1 and 100 characters long.',
+  [UploadNoteErrorType.INVALID_FILE_TYPE]: 'Please ensure all files uploaded are pdf files.',
+};
+
+export type UploadNoteIndexedErrors = Record<number, string[]>;
