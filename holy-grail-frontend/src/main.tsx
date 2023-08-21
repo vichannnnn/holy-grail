f962979c2cf946f021d4ceba0c36530e298c03ce
Raymond <raymond.goo124@gmail.com>
2023-08-21 20:57:27 +0800

    frontend/src/App.tsx: Include react-ga4

--- holy-grail-frontend/src/main.tsx
+++ holy-grail-frontend/src/main.tsx
@@ -5,6 +5,9 @@ import ReactDOM from 'react-dom/client';
 import { App } from './App.tsx';
 import { BrowserRouter } from 'react-router-dom';
 import './index.css';
+import ReactGA from 'react-ga4';
+
+ReactGA.initialize(import.meta.env.VITE_APP_GOOGLE_ANALYTICS_ID as string);
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
