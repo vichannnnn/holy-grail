a4c547b0302bdd7e6f2e62d3483368c2e3790236
hima <violet@himaa.me>
2023-08-26 17:02:21 +0800

    fix: fix backend validation for upload notes

--- holy-grail-backend/backend/app/models/library.py
+++ holy-grail-backend/backend/app/models/library.py
@@ -11,7 +11,7 @@ from sqlalchemy import func, ForeignKey, select, update, delete
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.orm import Mapped, mapped_column, relationship, selectinload
 from sqlalchemy.sql.expression import text
-from starlette.datastructures import UploadFile as StarletteUploadFile, FormData
+from starlette.datastructures import FormData
 
 from app.crud.base import CRUD
 from app.db.base_class import Base
@@ -92,39 +92,6 @@ class Library(Base, CRUD["Library"]):
     doc_type: Mapped["DocumentTypes"] = relationship(back_populates="documents")
 
     @classmethod
-    async def create_note(
-        cls,
-        session: AsyncSession,
-        uploaded_file: UploadFile,
-        uploaded_by: int,
-        data: NoteCreateSchema,
-        s3_bucket,
-    ):
-        if uploaded_file.content_type not in accepted_doc_type_extensions:
-            raise AppError.BAD_REQUEST_ERROR
-
-        if not isinstance(uploaded_file, StarletteUploadFile):
-            raise AppError.BAD_REQUEST_ERROR
-
-        extension = accepted_doc_type_extensions[uploaded_file.content_type]
-        file_id = uuid.uuid4().hex
-        file_name = file_id + extension
-        data_insert = NoteInsertSchema(
-            **data.dict(), uploaded_by=uploaded_by, file_name=file_name
-        )
-
-        try:
-            res = await super().create(session, data_insert.dict())
-        except SQLAlchemyExceptions.IntegrityError:
-            raise AppError.RESOURCES_ALREADY_EXISTS_ERROR
-
-        await session.refresh(
-            res, ["doc_category", "doc_type", "doc_subject", "account"]
-        )
-        await save_file(uploaded_file, file_name, s3_bucket)
-        return res
-
-    @classmethod
     async def create_many(
         cls,
         session: AsyncSession,
@@ -132,62 +99,26 @@ class Library(Base, CRUD["Library"]):
         uploaded_by: int,
         s3_bucket: boto3.client,
     ):
-        if len(form_data) > 25:
+        # A form has 5 fields, we have 25 limits so it should be 25 * 5
+        if len(form_data) > 25 * 5:
             raise AppError.BAD_REQUEST_ERROR
 
         valid_notes: List[tuple[NoteCreateSchema, int]] = []
         failed_notes = {
-            UploadError.DOCUMENT_NAME_DUPLICATED.name: [],
             UploadError.SCHEMA_VALIDATION_ERROR.name: [],
             UploadError.INVALID_FILE_TYPE.name: [],
-            UploadError.DOCUMENT_NAME_IN_DB.name: [],
         }
-
-        # Create an array to keep track of the indexes of the duplicate document names.
-        # Create a list of tuples where the first element is the document name and the second is the index.
-        track_duplicate_names = []
 
         document_names = [
             (form_data[f"{i}[name]"], i) for i in range(len(form_data) // 6)
         ]
 
-        # First check where we verify user input
-        # Go through the list and add the name of document names to 'duplicates', to verify error for subsequent docs.
-        # If the input passes this check, check for data structure validation and add into valid_notes, else pass error.
         for name, idx in document_names:
-            if name not in track_duplicate_names:
-                track_duplicate_names.append(name)
-
-                res, idx = form_data_note_parser(form_data, idx)
-                if res:
-                    valid_notes.append((res, idx))
-                else:
-                    failed_notes[UploadError.SCHEMA_VALIDATION_ERROR.name].append(idx)
-
+            res, idx = form_data_note_parser(form_data, idx)
+            if res:
+                valid_notes.append((res, idx))
             else:
-                failed_notes[UploadError.DOCUMENT_NAME_DUPLICATED.name].append(idx)
-
-        # Second check where we verify the valid notes with reference to the DB
-        all_valid_notes_names = {idx[0].document_name: idx[1] for idx in valid_notes}
-        stmt = select(cls.document_name).where(
-            cls.document_name.in_(
-                [doc_name for doc_name in all_valid_notes_names.keys()]
-            )
-        )
-        result = await session.execute(stmt)
-
-        idxes_to_remove = []
-        for conflicting_note_name in result.scalars().all():
-            idx = all_valid_notes_names[conflicting_note_name]
-            failed_notes[UploadError.DOCUMENT_NAME_IN_DB.name].append(idx)
-            idxes_to_remove.append(idx)
-
-        valid_notes: List[tuple[NoteCreateSchema, int]] = [
-            (elem[0], elem[1]) for elem in valid_notes if elem[1] not in idxes_to_remove
-        ]
-
-        # At this point, we have filtered out all the conflicting document names and validation errors.
-        # Now we are validating the file type of every non-filtered upload at this point
+                failed_notes[UploadError.SCHEMA_VALIDATION_ERROR.name].append(idx)
 
         idxes_to_remove = []
         for note, idx in valid_notes:
@@ -231,14 +162,9 @@ class Library(Base, CRUD["Library"]):
                     obj, ["doc_category", "doc_type", "doc_subject", "account"]
                 )
 
-        except SQLAlchemyExceptions.IntegrityError as exc:
+        except SQLAlchemyExceptions.IntegrityError:
             await session.rollback()
-            if str(exc).find("ForeignKeyViolationError") != -1:
-                raise AppError.RESOURCES_NOT_FOUND_ERROR
-            elif str(exc).find("UniqueViolationError") != -1:
-                raise AppError.RESOURCES_ALREADY_EXISTS_ERROR from exc
-            raise AppError.RESOURCES_ALREADY_EXISTS_ERROR from exc
-
+            raise AppError.RESOURCES_NOT_FOUND_ERROR
         return objs
 
     @classmethod
