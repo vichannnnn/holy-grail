8324b4296f7b91dadaac483048cd9f0fe6222d57
hima <violet@himaa.me>
2023-10-13 17:05:20 +0800

    tests: update tests for upload note

--- holy-grail-backend/backend/app/tests/api/library.py
+++ holy-grail-backend/backend/app/tests/api/library.py
@@ -1,7 +1,6 @@
 from fastapi.testclient import TestClient
-from fastapi.encoders import jsonable_encoder
+
 from app import schemas
-from io import BytesIO
 
 CREATE_URL = "/auth/create"
 GET_USER_URL = "/auth/get"
@@ -18,98 +17,171 @@ ADMIN_APPROVE_NOTES_URL = "/admin/approve"
 # -------- INSERT TEST --------
 
 
-# def test_create_note_with_verified_user_and_create_txt_and_create_duplicate_name(
-#     create_doc_type_subject_education_level,
-#     test_note_insert: schemas.library.NoteCreateSchema,
-#     test_note_insert_2: schemas.library.NoteCreateSchema,
-#     test_client_verified_user: TestClient,
-# ):
-#     file_content = BytesIO(b"Some file content")
-#
-#     payload = dict(test_note_insert)
-#     response = test_client_verified_user.post(
-#         NOTE_URL,
-#         files={
-#             "file": (
-#                 "test.pdf",
-#                 file_content,
-#                 "application/pdf",
-#             )
-#         },
-#         params={**payload},
-#     )
-#
-#     assert response.status_code == 200
-#
-#     note_response_json = response.json()
-#     user_response = test_client_verified_user.get(GET_USER_URL)
-#     user_response_json = user_response.json()
-#
-#     assert note_response_json["document_name"] == test_note_insert.document_name
-#     assert not note_response_json["approved"]
-#     assert note_response_json["category"] == note_response_json["doc_category"]["id"]
-#     assert note_response_json["subject"] == note_response_json["doc_subject"]["id"]
-#     assert note_response_json["type"] == note_response_json["doc_type"]["id"]
-#     assert (
-#         note_response_json["uploaded_by"] == user_response_json["user_id"]
-#         and user_response_json["user_id"] == note_response_json["account"]["user_id"]
-#     )
-#
-#     payload = dict(test_note_insert)
-#     response = test_client_verified_user.post(
-#         NOTE_URL,
-#         files={
-#             "file": (
-#                 "test.txt",
-#                 file_content,
-#                 "text/plain",
-#             )
-#         },
-#         params={**payload},
-#     )
-#
-#     assert response.status_code == 400
-#
-#     payload = dict(test_note_insert)
-#     response = test_client_verified_user.post(
-#         NOTE_URL,
-#         files={
-#             "file": (
-#                 "test.pdf",
-#                 file_content,
-#                 "application/pdf",
-#             )
-#         },
-#         params={**payload},
-#     )
-#
-#     assert response.status_code == 409
-#
-#     payload = dict(test_note_insert_2)
-#     response = test_client_verified_user.post(
-#         NOTE_URL,
-#         files={
-#             "file": (
-#                 "test.pdf",
-#                 file_content,
-#                 "application/pdf",
-#             )
-#         },
-#         params={**payload},
-#     )
-#
-#     note_response_json = response.json()
-#     user_response = test_client_verified_user.get(GET_USER_URL)
-#     user_response_json = user_response.json()
-#
-#     assert note_response_json["document_name"] == test_note_insert_2.document_name
-#     assert not note_response_json["approved"]
-#     assert note_response_json["category"] == note_response_json["doc_category"]["id"]
-#     assert note_response_json["subject"] == note_response_json["doc_subject"]["id"]
-#     assert note_response_json["type"] == note_response_json["doc_type"]["id"]
-#     assert user_response_json["user_id"] == note_response_json["account"]["user_id"]
-#
-#     assert response.status_code == 200
+def test_create_note_with_verified_user_and_create_two_notes_separately(
+    create_doc_type_subject_education_level,
+    test_note_insert: schemas.library.NoteCreateSchema,
+    test_note_insert_2: schemas.library.NoteCreateSchema,
+    test_client_verified_user: TestClient,
+):
+    files_1 = {
+        "0[file]": (
+            "document_name.pdf",
+            test_note_insert.file.getvalue(),
+            "application/pdf",
+        )
+    }
+
+    data_1 = {
+        "0[name]": test_note_insert.document_name,
+        "0[category]": test_note_insert.category,
+        "0[subject]": test_note_insert.subject,
+        "0[type]": test_note_insert.type,
+        "0[year]": test_note_insert.year if test_note_insert.year else "",
+    }
+
+    files_2 = {
+        "0[file]": (
+            "document_name2.pdf",
+            test_note_insert_2.file.getvalue(),
+            "application/pdf",
+        )
+    }
+
+    data_2 = {
+        "0[name]": test_note_insert_2.document_name,
+        "0[category]": test_note_insert_2.category,
+        "0[subject]": test_note_insert_2.subject,
+        "0[type]": test_note_insert_2.type,
+        "0[year]": 0,
+    }
+
+    response = test_client_verified_user.post(NOTE_URL, data=data_1, files=files_1)
+
+    assert response.status_code == 200
+
+    note_response_json = response.json()
+    user_response = test_client_verified_user.get(GET_USER_URL)
+    user_response_json = user_response.json()
+
+    assert note_response_json[0]["document_name"] == test_note_insert.document_name
+    assert not note_response_json[0]["approved"]
+    assert (
+        note_response_json[0]["category"] == note_response_json[0]["doc_category"]["id"]
+    )
+    assert (
+        note_response_json[0]["subject"] == note_response_json[0]["doc_subject"]["id"]
+    )
+    assert note_response_json[0]["type"] == note_response_json[0]["doc_type"]["id"]
+    assert (
+        note_response_json[0]["uploaded_by"] == user_response_json["user_id"]
+        and user_response_json["user_id"] == note_response_json[0]["account"]["user_id"]
+    )
+    assert note_response_json[0]["extension"] == ".pdf"
+
+    response = test_client_verified_user.post(
+        NOTE_URL,
+        data=data_2,
+        files=files_2,
+    )
+
+    note_response_json = response.json()
+    user_response = test_client_verified_user.get(GET_USER_URL)
+    user_response_json = user_response.json()
+
+    assert response.status_code == 200
+    assert note_response_json[0]["document_name"] == test_note_insert_2.document_name
+    assert not note_response_json[0]["approved"]
+    assert (
+        note_response_json[0]["category"] == note_response_json[0]["doc_category"]["id"]
+    )
+    assert (
+        note_response_json[0]["subject"] == note_response_json[0]["doc_subject"]["id"]
+    )
+    assert note_response_json[0]["type"] == note_response_json[0]["doc_type"]["id"]
+    assert user_response_json["user_id"] == note_response_json[0]["account"]["user_id"]
+    assert note_response_json[0]["extension"] == ".pdf"
+
+
+def test_create_note_with_verified_user_and_create_two_notes_at_once(
+    create_doc_type_subject_education_level,
+    test_note_insert: schemas.library.NoteCreateSchema,
+    test_note_insert_2: schemas.library.NoteCreateSchema,
+    test_client_verified_user: TestClient,
+):
+    files_1 = {
+        "0[file]": (
+            "document_name.pdf",
+            test_note_insert.file.getvalue(),
+            "application/pdf",
+        )
+    }
+
+    data_1 = {
+        "0[name]": test_note_insert.document_name,
+        "0[category]": test_note_insert.category,
+        "0[subject]": test_note_insert.subject,
+        "0[type]": test_note_insert.type,
+        "0[year]": test_note_insert.year if test_note_insert.year else "",
+    }
+
+    files_2 = {
+        "1[file]": (
+            "document_name2.pdf",
+            test_note_insert_2.file.getvalue(),
+            "application/pdf",
+        )
+    }
+
+    data_2 = {
+        "1[name]": test_note_insert_2.document_name,
+        "1[category]": test_note_insert_2.category,
+        "1[subject]": test_note_insert_2.subject,
+        "1[type]": test_note_insert_2.type,
+        "1[year]": 0,
+    }
+
+    response = test_client_verified_user.post(
+        NOTE_URL, data={**data_1, **data_2}, files={**files_1, **files_2}
+    )
+
+    assert response.status_code == 200
+
+    note_response_json = response.json()
+    user_response = test_client_verified_user.get(GET_USER_URL)
+    user_response_json = user_response.json()
+
+    assert note_response_json[0]["document_name"] == test_note_insert.document_name
+    assert not note_response_json[0]["approved"]
+    assert (
+        note_response_json[0]["category"] == note_response_json[0]["doc_category"]["id"]
+    )
+    assert (
+        note_response_json[0]["subject"] == note_response_json[0]["doc_subject"]["id"]
+    )
+    assert note_response_json[0]["type"] == note_response_json[0]["doc_type"]["id"]
+    assert (
+        note_response_json[0]["uploaded_by"] == user_response_json["user_id"]
+        and user_response_json["user_id"] == note_response_json[0]["account"]["user_id"]
+    )
+    assert note_response_json[0]["extension"] == ".pdf"
+
+    assert note_response_json[1]["document_name"] == test_note_insert_2.document_name
+    assert not note_response_json[1]["approved"]
+    assert (
+        note_response_json[1]["category"] == note_response_json[1]["doc_category"]["id"]
+    )
+    assert (
+        note_response_json[1]["subject"] == note_response_json[1]["doc_subject"]["id"]
+    )
+    assert note_response_json[1]["type"] == note_response_json[1]["doc_type"]["id"]
+    assert user_response_json["user_id"] == note_response_json[1]["account"]["user_id"]
+    assert (
+        note_response_json[1]["uploaded_by"] == user_response_json["user_id"]
+        and user_response_json["user_id"] == note_response_json[1]["account"]["user_id"]
+    )
+    assert note_response_json[1]["extension"] == ".pdf"
+
+
 #
 #
 # def test_create_note_for_category_does_not_exist(
