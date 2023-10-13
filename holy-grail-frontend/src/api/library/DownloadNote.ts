c47618d1f5221f73a844a61c32091100dcc4ab10
hima <violet@himaa.me>
2023-10-13 17:36:43 +0800

    fix: fix issue where downloaded file is in txt

--- holy-grail-frontend/src/api/library/DownloadNote.ts
+++ holy-grail-frontend/src/api/library/DownloadNote.ts
@@ -1,23 +1,17 @@
 import { apiClient } from '@apiClient';
 import { Note } from './types';
 
-const getExtensionFromFilename = (filename: string): string => {
-  const parts = filename.split('.');
-  return parts.length > 1 ? `.${parts.pop()}` : '';
-};
-
 export const downloadNote = async (note: Note) => {
   const response = await apiClient.get(`/note/download/${note.id}`, {
     responseType: 'arraybuffer',
   });
-  const extension = getExtensionFromFilename(note.file_name);
 
   if (response.status === 200) {
     const blob = new Blob([response.data]);
     const blobUrl = URL.createObjectURL(blob);
     const link = document.createElement('a');
     link.href = blobUrl;
-    link.download = `${note.document_name}${extension}`;
+    link.download = `${note.document_name}${note.extension}`;
     link.click();
     URL.revokeObjectURL(blobUrl);
   } else {
