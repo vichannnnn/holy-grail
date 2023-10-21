39635191d72981ca6487905eee7b94d998f0b4a7
hima <violet@himaa.me>
2023-10-22 00:56:39 +0800

    feat: update landing page to add analytics section

---
+++ holy-grail-frontend/src/features/Landing/Analytics.tsx
@@ -0,0 +1,45 @@
+import React, { useState, useEffect } from 'react';
+import CountUp from 'react-countup';
+import { AnalyticsResponse, getAnalytics } from '@api/analytics';
+import './Analytics.css';
+
+export const Analytics = () => {
+  const [analyticsData, setAnalyticsData] = useState<AnalyticsResponse | null>(null);
+
+  useEffect(() => {
+    const fetchAnalytics = async () => {
+      const data = await getAnalytics();
+      setAnalyticsData(data);
+    };
+    fetchAnalytics().then((r) => null);
+  }, []);
+
+  return (
+    <div className='analytics-container'>
+      <div className='analytics-item'>
+        <span className='analytics-title'>Notes Downloaded</span>
+        <span className='analytics-value'>
+          {analyticsData ? (
+            <CountUp start={0} end={analyticsData.file_download_count} separator=',' />
+          ) : null}
+        </span>
+      </div>
+      <div className='analytics-item'>
+        <span className='analytics-title'>Students</span>
+        <span className='analytics-value'>
+          {analyticsData ? (
+            <CountUp start={0} end={analyticsData.unique_active_users} separator=',' />
+          ) : null}
+        </span>
+      </div>
+      <div className='analytics-item'>
+        <span className='analytics-title'>Accounts Created</span>
+        <span className='analytics-value'>
+          {analyticsData ? (
+            <CountUp start={0} end={analyticsData.user_count} separator=',' />
+          ) : null}
+        </span>
+      </div>
+    </div>
+  );
+};
---
+++ holy-grail-frontend/src/features/Landing/Hero.tsx
@@ -0,0 +1,57 @@
+import { useContext } from 'react';
+import { MediaQueryContext } from '@providers';
+import { useNavigation } from '@utils';
+import './Hero.css';
+
+export const Hero = () => {
+  const { goToLibrary, goToRegister } = useNavigation();
+  const { isDesktop } = useContext(MediaQueryContext);
+
+  return (
+    <div className='outer-hero-container'>
+      <div className='inner-hero-container'>
+        {!isDesktop && (
+          <div className='right-hero'>
+            <img src='/grail-chan-studying.png' alt='Studying' width='450' />
+          </div>
+        )}
+        <div className='left-hero'>
+          <div>
+            <h1>Access your notes you need in just a click.</h1>
+            <p>
+              A completely free-to-access web library aimed at Singaporean students that houses all
+              the relevant summary notes and practice papers for GCE 'O' Levels, GCE 'A' Levels and
+              International Baccalaureate.
+            </p>
+            <button className='try-btn' onClick={goToLibrary}>
+              Click here to access the library!
+            </button>
+          </div>
+        </div>
+        {isDesktop && (
+          <div className='right-hero'>
+            <img src='/grail-chan-studying.png' alt='Studying' width='450' />
+          </div>
+        )}
+      </div>
+      <div className='inner-hero-container'>
+        <div className='left-hero'>
+          <img src='/grail-chan-sparkling.png' alt='Sparkling' width='450' />
+        </div>
+        <div className='right-hero'>
+          <div>
+            <h1>Want to be a contributor?</h1>
+            <p>
+              Accessing notes is free for everyone, even for those without an account, but if you
+              want to contribute revision materials into the repository, you can log in or sign up
+              for an account below to start contributing!
+            </p>
+            <button className='signup-btn' onClick={goToRegister}>
+              Click here to sign up!
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
