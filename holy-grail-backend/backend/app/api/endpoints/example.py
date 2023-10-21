803d8d5f3b0597d99ebdb730afcda9cdd39efb63
hima <violet@himaa.me>
2023-10-21 23:40:52 +0800

    feat: add analytics endpoint

--- holy-grail-backend/backend/app/api/endpoints/example.py
+++ holy-grail-backend/backend/app/api/endpoints/example.py
@@ -1,8 +1,18 @@
 from fastapi import APIRouter
 
+from app.api.deps import CurrentSession
+from app.models.analytics import Analytics
+from app.schemas.analytics import AnalyticsResponse
+
 router = APIRouter()
 
 
 @router.get("/hello")
 async def sanity_check():
     return {"Hello": "World!"}
+
+
+@router.get("/analytics", response_model=AnalyticsResponse)
+async def get_analytics(session: CurrentSession):
+    response = await Analytics.get_latest_analytics(session)
+    return response
