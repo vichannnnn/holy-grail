61e8c8223883aec330ec13b991b9e6d502eedafc
hima <violet@himaa.me>
2023-11-24 02:12:48 +0800

    chore: remove unnecessary code, files and components

--- holy-grail-frontend/src/components/HeroBox/HeroBox.tsx
+++
@@ -1,9 +0,0 @@
-import { Box, BoxProps } from '@mui/material';
-
-export const HeroBox = ({ children, ...props }: BoxProps) => {
-  return (
-    <Box sx={{ width: '30%' }} {...props}>
-      {children}
-    </Box>
-  );
-};
