540bdc736056dc771ad35ff19da52269e57404dc
hima <violet@himaa.me>
2023-11-25 19:23:58 +0800

    feat: update hero description

--- holy-grail-frontend/src/features/Landing/Hero.tsx
+++ holy-grail-frontend/src/features/Landing/Hero.tsx
@@ -25,9 +25,9 @@ export const Hero = () => {
           <div>
             <h1>Access your notes you need in just a click.</h1>
             <p>
-              A completely free-to-access web library aimed at Singaporean students that houses all
-              the relevant summary notes and practice papers for GCE 'O' Levels, GCE 'A' Levels and
-              International Baccalaureate.
+              Holy Grail is a completely free-to-access web library aimed at Singaporean students
+              that houses all the summary notes and practice papers for GCE 'O' Levels, GCE 'A'
+              Levels and International Baccalaureate.
             </p>
             <Button onClick={goToLibrary}>
               Head to the Library <ArrowForwardIcon style={{ verticalAlign: 'middle' }} />
@@ -56,9 +56,9 @@ export const Hero = () => {
           <div>
             <h1>Want to contribute?</h1>
             <p>
-              Accessing notes is free for everyone, even for those without an account, but if you
-              want to contribute revision materials into the repository, you can log in or sign up
-              for an account below to start contributing!
+              Accessing notes is free for everyone in Holy Grail, even for those without an account,
+              but if you want to contribute revision materials into the repository, you can log in
+              or sign up for an account below to start contributing!
             </p>
             <Button onClick={goToRegister}>Click here to sign up!</Button>
           </div>
