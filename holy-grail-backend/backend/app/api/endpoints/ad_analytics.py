2cf8894875c4c123a9b7afdc1fe71482c12cf24e
SebassNoob <sebastian.ong@hotmail.com>
2023-11-26 22:06:18 +0800

    feat: add ratelimits, auth for ad analytics

--- holy-grail-backend/backend/app/api/endpoints/ad_analytics.py
+++ holy-grail-backend/backend/app/api/endpoints/ad_analytics.py
@@ -1,30 +1,34 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Request
 
-from app.api.deps import CurrentSession
+from app.api.deps import CurrentSession, SessionDeveloper
 from app.models.ad_analytics import AdAnalytics
+from app.utils.limiter import conditional_rate_limit
 
 router = APIRouter()
 
 
 @router.post("/new_day")
-async def new_day(session: CurrentSession):
+@conditional_rate_limit("1/23hour")
+async def new_day(request: Request, session: CurrentSession):
     await AdAnalytics.new_day(session)
     return {"status": "success"}
 
 
 @router.patch("/ad_click")
-async def ad_click(session: CurrentSession):
+@conditional_rate_limit("1/day")
+async def ad_click(request: Request, session: CurrentSession):
     await AdAnalytics.ad_click(session)
     return {"status": "success"}
 
 
 @router.patch("/ad_view")
-async def ad_view(session: CurrentSession):
+@conditional_rate_limit("1/day")
+async def ad_view(request: Request, session: CurrentSession):
     await AdAnalytics.ad_view(session)
     return {"status": "success"}
 
 
 @router.get("/get_latest_ad_analytics")
-async def get_latest_ad_analytics(session: CurrentSession):
+async def get_latest_ad_analytics(session: SessionDeveloper):
     latest_ad_analytics = await AdAnalytics.get_latest_ad_analytics(session)
     return {"status": "success", "data": latest_ad_analytics}
