ad1279d5eb4774903002dffb0767a848e7c3b712
hima <violet@himaa.me>
2023-11-26 16:48:58 +0800

    feat: update mobile design for upload page

--- holy-grail-frontend/src/features/Upload/UploadNote/MobileUploadNote.tsx
+++ holy-grail-frontend/src/features/Upload/UploadNote/MobileUploadNote.tsx
@@ -2,10 +2,9 @@ import { useState, useContext, useEffect } from 'react';
 import { Controller } from 'react-hook-form';
 import { fetchCategory, SubjectType, fetchAllSubjects } from '@api/library';
 import { Combobox, DropdownMenuItem } from '@components';
-import { UploadNoteProps } from '@features';
+import { NoteOptionsIcon, UploadNoteProps } from '@features';
 import { AuthContext } from '@providers';
-import { TextField, Grid, Collapse, Box, IconButton, Menu } from '@mui/material';
-import MoreHorizIcon from '@mui/icons-material/MoreHoriz';
+import { TextField, Menu } from '@mui/material';
 import './MobileUploadNote.css';
 
 export const MobileUploadNote = ({
@@ -49,142 +48,116 @@ export const MobileUploadNote = ({
   }, [categoryValue]);
 
   return (
-    <div className='mobile-upload-note-container' style={{ width: '90vw' }}>
-      <Grid
-        container
-        sx={{
-          border: '1px solid black',
-          borderRadius: '10px',
-        }}
-      >
-        <Grid container item className='grid-container'>
-          <a className='file-name-expanded'>{field.file.name}</a>
+    <div className='mobile-upload-note-container'>
+      <div className='single-note-container'>
+        <div className='file-name-header-container'>
+          <a>{field.file.name}</a>
+          <div style={{ display: 'flex', flexDirection: 'row', marginRight: '16px' }}>
+            <NoteOptionsIcon onClick={(event) => setAnchorEl(event.currentTarget)} />
+          </div>
+        </div>
+        <div className='combobox-container-mobile'>
+          <Controller
+            name={`notes.${index}.name`}
+            control={control}
+            defaultValue={field.name}
+            render={({ field }) => (
+              <TextField
+                className='note-combobox'
+                label='Document Name'
+                placeholder={`Enter document name (eg. ${user?.username || 'anonymous'}'s Notes)`}
+                variant='outlined'
+                value={field.value}
+                onChange={field.onChange}
+                error={Boolean(errors && errors.name)}
+                helperText={errors && errors.name ? errors.name.message : ' '}
+              />
+            )}
+          />
+          <Controller
+            name={`notes.${index}.category`}
+            control={control}
+            defaultValue={field.category}
+            render={({ field }) => (
+              <Combobox
+                className='note-combobox'
+                label='Category'
+                value={field.value}
+                onChange={async (newValue) => {
+                  field.onChange(newValue);
+                }}
+                options={
+                  options?.categories.map((category) => ({
+                    id: category.id,
+                    name: category.name,
+                  })) ?? []
+                }
+                error={Boolean(errors && errors.category)}
+                helperText={errors && errors.category ? errors.category.message : ' '}
+              />
+            )}
+          />
+          <Controller
+            name={`notes.${index}.subject`}
+            control={control}
+            defaultValue={field.subject}
+            render={({ field }) => (
+              <Combobox
+                className='note-combobox'
+                label='Subject'
+                value={field.value}
+                onChange={field.onChange}
+                options={subjectsData}
+                disabled={!categoryValue || categoryValue === 0}
+                error={Boolean(errors && errors.subject)}
+                helperText={errors && errors.subject ? errors.subject.message : ' '}
+              />
+            )}
+          />
+          <Controller
+            name={`notes.${index}.type`}
+            control={control}
+            defaultValue={field.type}
+            render={({ field }) => (
+              <Combobox
+                className='note-combobox'
+                label='Type'
+                value={field.value}
+                onChange={field.onChange}
+                options={
+                  options?.types.map((type) => ({
+                    id: type.id,
+                    name: type.name,
+                  })) ?? []
+                }
+                error={Boolean(errors && errors.type)}
+                helperText={errors && errors.type ? errors.type.message : ' '}
+              />
+            )}
+          />
+          <Controller
+            name={`notes.${index}.year`}
+            control={control}
+            defaultValue={field.year}
+            render={({ field }) => (
+              <Combobox
+                className='note-combobox'
+                label='Year'
+                value={field.value ?? ''}
+                onChange={field.onChange}
+                options={
+                  options?.years.map((year) => ({
+                    id: year.id,
+                    name: year.name,
+                  })) ?? []
+                }
+                helperText=' '
+              />
+            )}
+          />
+        </div>
+      </div>
 
-          <IconButton
-            onClick={(event) => setAnchorEl(event.currentTarget)}
-            style={{ position: 'absolute', top: '8px', right: '8px', padding: 0 }}
-          >
-            <MoreHorizIcon />
-          </IconButton>
-        </Grid>
-
-        <Collapse in={true} timeout='auto' unmountOnExit className='collapsible-grid'>
-          <Box className='outer-note-box'>
-            <Box className='inner-note-box'>
-              <Grid container item className='combobox-container-mobile'>
-                <Grid item xs={12}>
-                  <Grid container item className='document-name-grid-container'>
-                    <Controller
-                      name={`notes.${index}.name`}
-                      control={control}
-                      defaultValue={field.name}
-                      render={({ field }) => (
-                        <TextField
-                          className='document-name-text-field'
-                          label='Document Name'
-                          placeholder={`Enter document name (eg. ${
-                            user?.username || 'anonymous'
-                          }'s Notes)`}
-                          variant='outlined'
-                          value={field.value}
-                          onChange={field.onChange}
-                          error={Boolean(errors && errors.name)}
-                          helperText={errors && errors.name ? errors.name.message : ' '}
-                        />
-                      )}
-                    />
-                  </Grid>
-
-                  <Controller
-                    name={`notes.${index}.category`}
-                    control={control}
-                    defaultValue={field.category}
-                    render={({ field }) => (
-                      <Combobox
-                        className='note-combobox'
-                        label='Category'
-                        value={field.value}
-                        onChange={async (newValue) => {
-                          field.onChange(newValue);
-                        }}
-                        options={
-                          options?.categories.map((category) => ({
-                            id: category.id,
-                            name: category.name,
-                          })) ?? []
-                        }
-                        error={Boolean(errors && errors.category)}
-                        helperText={errors && errors.category ? errors.category.message : ' '}
-                      />
-                    )}
-                  />
-                  <Controller
-                    name={`notes.${index}.subject`}
-                    control={control}
-                    defaultValue={field.subject}
-                    render={({ field }) => (
-                      <Combobox
-                        className='note-combobox'
-                        label='Subject'
-                        value={field.value}
-                        onChange={field.onChange}
-                        options={subjectsData}
-                        disabled={!categoryValue || categoryValue === 0}
-                        error={Boolean(errors && errors.subject)}
-                        helperText={errors && errors.subject ? errors.subject.message : ' '}
-                      />
-                    )}
-                  />
-
-                  <Controller
-                    name={`notes.${index}.type`}
-                    control={control}
-                    defaultValue={field.type}
-                    render={({ field }) => (
-                      <Combobox
-                        className='note-combobox'
-                        label='Type'
-                        value={field.value}
-                        onChange={field.onChange}
-                        options={
-                          options?.types.map((type) => ({
-                            id: type.id,
-                            name: type.name,
-                          })) ?? []
-                        }
-                        error={Boolean(errors && errors.type)}
-                        helperText={errors && errors.type ? errors.type.message : ' '}
-                      />
-                    )}
-                  />
-
-                  <Controller
-                    name={`notes.${index}.year`}
-                    control={control}
-                    defaultValue={field.year}
-                    render={({ field }) => (
-                      <Combobox
-                        className='note-combobox'
-                        label='Year'
-                        value={field.value ?? ''}
-                        onChange={field.onChange}
-                        options={
-                          options?.years.map((year) => ({
-                            id: year.id,
-                            name: year.name,
-                          })) ?? []
-                        }
-                        helperText=' '
-                      />
-                    )}
-                  />
-                </Grid>
-                ,
-              </Grid>
-            </Box>
-          </Box>
-        </Collapse>
-      </Grid>
       <Menu
         anchorEl={anchorEl}
         open={Boolean(anchorEl)}
