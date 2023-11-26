411922fc3a7163b7446ff40f65a037a472b1317b
hima <violet@himaa.me>
2023-11-26 19:00:38 +0800

    feat: update hero component

--- holy-grail-frontend/src/features/Landing/Hero/MobileHero.tsx
+++ holy-grail-frontend/src/features/Landing/Hero/MobileHero.tsx
@@ -7,47 +7,31 @@ export const MobileHero = () => {
   const { goToLibrary, goToRegister } = useNavigation();
 
   return (
-    <div className='mobile-outer-hero-container'>
-      <div className='mobile-inner-hero-container'>
-        <div className='mobile-right-hero'>
-          <img
-            src='https://document.grail.moe/grail-chan-studying.png'
-            alt='Studying'
-            width='450'
-          />
-        </div>
-        <div className='mobile-left-hero'>
-          <div>
-            <h1>Access your notes you need in just a click.</h1>
-            <p>
-              Holy Grail is a completely free-to-access web library aimed at Singaporean students
-              that houses all the summary notes and practice papers for GCE 'O' Levels, GCE 'A'
-              Levels and International Baccalaureate.
-            </p>
-            <Button onClick={goToLibrary}>
-              Head to the Library <ArrowForwardIcon style={{ verticalAlign: 'middle' }} />
-            </Button>
-          </div>
+    <div className='mobile-hero-container'>
+      <div>
+        <img src='https://document.grail.moe/grail-chan-studying.png' alt='Studying' />
+        <h1>Access your notes you need in just a click.</h1>
+        <p>
+          Holy Grail is a completely free-to-access web library aimed at Singaporean students that
+          houses all the summary notes and practice papers for GCE 'O' Levels, GCE 'A' Levels and
+          International Baccalaureate.
+        </p>
+        <div className='hero-button-container'>
+          <Button onClick={goToLibrary}>
+            Head to the Library <ArrowForwardIcon style={{ verticalAlign: 'middle' }} />
+          </Button>
         </div>
       </div>
-      <div className='mobile-inner-hero-container'>
-        <div className='mobile-left-hero'>
-          <img
-            src='https://document.grail.moe/grail-chan-sparkling.png'
-            alt='Sparkling'
-            width='450'
-          />
-        </div>
-        <div className='mobile-right-hero'>
-          <div>
-            <h1>Want to contribute?</h1>
-            <p>
-              Accessing notes is free for everyone in Holy Grail, even for those without an account,
-              but if you want to contribute revision materials into the repository, you can log in
-              or sign up for an account below to start contributing!
-            </p>
-            <Button onClick={goToRegister}>Click here to sign up!</Button>
-          </div>
+      <div>
+        <img src='https://document.grail.moe/grail-chan-sparkling.png' alt='Sparkling' />
+        <h1>Want to contribute?</h1>
+        <p>
+          Accessing notes is free for everyone in Holy Grail, even for those without an account, but
+          if you want to contribute revision materials into the repository, you can log in or sign
+          up for an account below to start contributing!
+        </p>
+        <div className='hero-button-container'>
+          <Button onClick={goToRegister}>Click here to sign up!</Button>
         </div>
       </div>
     </div>
