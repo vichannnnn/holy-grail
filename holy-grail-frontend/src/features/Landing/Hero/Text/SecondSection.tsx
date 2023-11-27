dc4c55e9badd4b059aa0a8ee8a7362251048c8c8
hima <violet@himaa.me>
2023-11-28 00:24:18 +0800

    feat: add fade in

--- holy-grail-frontend/src/features/Landing/Hero/Text/SecondSection.tsx
+++ holy-grail-frontend/src/features/Landing/Hero/Text/SecondSection.tsx
@@ -1,12 +1,14 @@
+import FadeIn from 'react-fade-in';
+
 export const SecondSection = () => {
   return (
-    <>
+    <FadeIn>
       <h1>Want to contribute?</h1>
       <p>
         Accessing notes is free for everyone in Holy Grail, even for those without an account, but
         if you want to contribute revision materials into the repository, you can log in or sign up
         for an account below to start contributing!
       </p>
-    </>
+    </FadeIn>
   );
 };
