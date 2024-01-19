3847aac70276584a5a674a6896ed6099bd133a0e
vi.channnnn <violet@himaa.me>
2024-01-20 00:07:07 +0800

    feat: rename ads to showcase and remove current ongoing ads

--- holy-grail-frontend/src/features/Footer/Footer.tsx
+++ holy-grail-frontend/src/features/Footer/Footer.tsx
@@ -1,59 +1,59 @@
 import { useContext } from 'react';
 import { Link as RouterLink } from 'react-router-dom';
 import { AuthContext } from '@providers';
-import { FooterAds } from '@features';
+import { FooterShowcase } from '@features';
 import './Footer.css';
 
 export const Footer = () => {
   const { user } = useContext(AuthContext);
 
   return (
-    <div className='footer-with-ads'>
-      <FooterAds />
-      <footer className='footer'>
-        <div className='footer-logo-section'>
-          <img
-            className='footer-logo-image'
-            src='https://document.grail.moe/grail-chan-happy.png'
-            alt=''
-          />
-        </div>
-        <div className='footer-container'>
-          <div>
-            <div className='footer-title'>Information</div>
-            <div className='footer-content'>
-              <RouterLink to='/#home'>Home</RouterLink>
-            </div>
-            <div className='footer-content'>
-              <RouterLink to='/library'>Library</RouterLink>
-            </div>
-            <div className='footer-content'>
-              <RouterLink to='/#FAQ'>FAQ</RouterLink>
-            </div>
+    // <div className='footer-with-showcase'>
+    //   <FooterShowcase />
+    <footer className='footer'>
+      <div className='footer-logo-section'>
+        <img
+          className='footer-logo-image'
+          src='https://document.grail.moe/grail-chan-happy.png'
+          alt=''
+        />
+      </div>
+      <div className='footer-container'>
+        <div>
+          <div className='footer-title'>Information</div>
+          <div className='footer-content'>
+            <RouterLink to='/#home'>Home</RouterLink>
           </div>
-          <div>
-            <div className='footer-title'>Contribute</div>
-            <div className='footer-content'>
-              <RouterLink to='/upload'>Upload</RouterLink>
-            </div>
+          <div className='footer-content'>
+            <RouterLink to='/library'>Library</RouterLink>
           </div>
-          <div>
-            <div className='footer-title'>Account</div>
-            <div className='footer-content' style={user ? { display: 'none' } : undefined}>
-              <RouterLink to='/login'>Log In</RouterLink>
-            </div>
-            <div className='footer-content' style={user ? { display: 'none' } : undefined}>
-              <RouterLink to='/register'>Register</RouterLink>
-            </div>
-            <div className='footer-content' style={user ? undefined : { display: 'none' }}>
-              <RouterLink to='/account'>Account Settings</RouterLink>
-            </div>
+          <div className='footer-content'>
+            <RouterLink to='/#FAQ'>FAQ</RouterLink>
           </div>
         </div>
-        <div className='footer-copyright-section'>
-          &#169; 2023 Holy Grail Team • Questions? Contact us at grail@himaa.me
+        <div>
+          <div className='footer-title'>Contribute</div>
+          <div className='footer-content'>
+            <RouterLink to='/upload'>Upload</RouterLink>
+          </div>
         </div>
-      </footer>
-    </div>
+        <div>
+          <div className='footer-title'>Account</div>
+          <div className='footer-content' style={user ? { display: 'none' } : undefined}>
+            <RouterLink to='/login'>Log In</RouterLink>
+          </div>
+          <div className='footer-content' style={user ? { display: 'none' } : undefined}>
+            <RouterLink to='/register'>Register</RouterLink>
+          </div>
+          <div className='footer-content' style={user ? undefined : { display: 'none' }}>
+            <RouterLink to='/account'>Account Settings</RouterLink>
+          </div>
+        </div>
+      </div>
+      <div className='footer-copyright-section'>
+        &#169; 2023 Holy Grail Team • Questions? Contact us at grail@himaa.me
+      </div>
+    </footer>
+    // </div>
   );
 };
