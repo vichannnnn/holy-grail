03c85367d95dc1760a21176a162cb0dda6a598af
vi.channnnn <violet@himaa.me>
2024-02-13 15:37:42 +0900

    feat(landing-page): add banner for landing

--- holy-grail-frontend/src/features/Landing/LandingPage.tsx
+++ holy-grail-frontend/src/features/Landing/LandingPage.tsx
@@ -2,11 +2,15 @@ import { Analytics } from './Analytics';
 import { FAQ } from './FAQ';
 import { Hero } from './Hero';
 import './LandingPage.css';
+import { IllumeShowcase } from '../Showcase';
 
 export const LandingPage = () => {
   return (
     <div className='landing-page'>
       <Hero />
+      <div className='illume-landing-page-showcase'>
+        <IllumeShowcase />
+      </div>
       <Analytics />
       <FAQ />
     </div>
