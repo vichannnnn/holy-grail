e3c2e43f6ad5bc557901de406728e50614768221
vi.channnnn <violet@himaa.me>
2024-08-27 19:41:54 +0900

    feat: update frontend for analytics

--- holy-grail-backend/backend/app/api/endpoints/analytics.py
+++ holy-grail-backend/backend/app/api/endpoints/analytics.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter, Request
 
 from app.api.deps import CurrentSession
 from app.models.analytics import Analytics
+from app.schemas.analytics import AnalyticsResponse
 from app.utils.limiter import conditional_rate_limit
 
 router = APIRouter()
@@ -14,7 +15,7 @@ async def fetch_google_analytics(request: Request, session: CurrentSession):
     return resp
 
 
-@router.get("/get_latest_analytics")
+@router.get("/get_latest_analytics", response_model=AnalyticsResponse)
 async def ad_view(session: CurrentSession):
     resp = await Analytics.get_latest_analytics(session)
     return resp
