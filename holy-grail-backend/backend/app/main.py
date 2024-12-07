25fe601ba52175d7352ec3591b92772730d97cb6
Eugene <violet@himaa.me>
2024-12-08 02:14:50 +0800

    fix: fix vars

--- holy-grail-backend/backend/app/main.py
+++ holy-grail-backend/backend/app/main.py
@@ -23,9 +23,12 @@ app.add_exception_handler(RateLimitExceeded, _rate_limit_exceeded_handler)
 
 app.add_middleware(
     cors.CORSMiddleware,
-    allow_origins=ast.literal_eval(os.getenv("ORIGINS"))
-    if os.getenv("PRODUCTION") == "true"
-    else ["*"],
+    allow_origins=[
+        "http://localhost:3000",
+        "http://localhost:5173",
+        "https://grail.moe",
+        "https://dev.grail.moe"
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
