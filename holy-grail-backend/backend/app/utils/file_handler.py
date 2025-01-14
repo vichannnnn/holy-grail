8b0df2f2ab221f950af36d228ac8d7e00192895c
Eugene <violet@himaa.me>
2025-01-14 17:47:38 +0800

    chore: foramt

--- holy-grail-backend/backend/app/utils/file_handler.py
+++ holy-grail-backend/backend/app/utils/file_handler.py
@@ -4,7 +4,6 @@ from io import BytesIO
 import boto3
 from starlette.datastructures import UploadFile
 
-
 AWS_ACCESS_KEY = os.environ.get("AWS_ACCESS_KEY")
 AWS_SECRET_KEY = os.environ.get("AWS_SECRET_KEY")
 S3_BUCKET_NAME = (
