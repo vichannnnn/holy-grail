488587ac04340606ed76e51dba34377acadf74da
Eugene <violet@himaa.me>
2025-01-16 19:56:54 +0800

    fix(celery): update backend container url

--- holy-grail-backend/backend/app/tasks/fetch_google_analytics.py
+++ holy-grail-backend/backend/app/tasks/fetch_google_analytics.py
@@ -4,10 +4,11 @@ import requests
 
 from app.utils.worker import celery_app
 
+BACKEND_CONTAINER_URL = os.getenv("BACKEND_CONTAINER_URL", "http://localhost:8000")
 
 @celery_app.task(name="fetch_google_analytics")
 def fetch_google_analytics() -> None:
     resp = requests.post(
-        f"http://{os.environ['BACKEND_CONTAINER_URL']}/analytics/fetch_google_analytics"
+        f"{BACKEND_CONTAINER_URL}/analytics/fetch_google_analytics"
     )
     return resp.json()
--- holy-grail-backend/backend/app/tasks/health_check.py
+++ holy-grail-backend/backend/app/tasks/health_check.py
@@ -4,8 +4,9 @@ import requests
 
 from app.utils.worker import celery_app
 
+BACKEND_CONTAINER_URL = os.getenv("BACKEND_CONTAINER_URL", "http://localhost:8000")
 
 @celery_app.task(name="ping")
 def ping() -> None:
-    resp = requests.get(f"http://{os.environ['BACKEND_CONTAINER_URL']}/hello")
+    resp = requests.get(f"{BACKEND_CONTAINER_URL}/hello")
     return resp.json()
