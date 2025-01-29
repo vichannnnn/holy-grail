6a37aa60cb3ee1e9e5849175809d97983fe96c65
Eugene <violet@himaa.me>
2025-01-30 06:42:02 +0800

    test

--- terraform/rds/main.tf
+++ terraform/rds/main.tf
@@ -7,7 +7,7 @@ resource "aws_security_group" "rds_sg" {
     from_port   = 5432
     to_port     = 5432
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"] # Open to the public
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
