21c6583c335c8927278ce4d3fa40cce9d0822850
Eugene <violet@himaa.me>
2025-03-12 01:47:14 +0900

    chore: update footer

--- new-frontend/src/components/layouts/Footer/Footer.tsx
+++ new-frontend/src/components/layouts/Footer/Footer.tsx
@@ -6,16 +6,17 @@ const LeftSection = () => {
     <div className='flex flex-col gap-4'>
       <Link href='/' passHref>
         <img
-          src='/HimariLogoPrimaryLilac.png'
-          alt='Footer Logo'
-          className='w-40 h-auto md:w-60 md:h-auto lg:w-72 lg:h-auto'
+          src='https://image.himaa.me/grail-chan-happy-v1.webp'
+          alt='Holy Grail'
+          height='96'
+          width='96'
         />
       </Link>
       <div className='w-full max-w-md mt-4'>
         <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque a turpis nunc. In
-          ultrices malesuada ante, et porta augue mollis porttitor. Vestibulum sed lorem vel elit
-          porttitor dignissim. Maecenas vitae mauris risus.
+          Holy Grail is a completely free-to-access web library aimed at Singaporean students that
+          houses all the summary notes and practice papers for GCE &apos;O&apos; Levels, GCE
+          &apos;A&apos; Levels and International Baccalaureate.
         </p>
       </div>
     </div>
@@ -27,8 +28,20 @@ const RightSection = () => {
   return (
     <div className='flex flex-col md:mx-auto'>
       <div className='flex flex-col gap-4 md:flex-row md:gap-12'>
-        <div className='flex flex-col gap-3'>
-          <h3 className='font-bold text-hima-dark dark:text-hima-white'>Company</h3>
+        <div className='flex flex-col gap-2'>
+          <h3 className='font-bold text-hima-dark dark:text-hima-white'>Information</h3>
+          <Link href='/' passHref>
+            <p>Home</p>
+          </Link>
+          <Link href='/library' passHref>
+            <p>Library</p>
+          </Link>
+          <Link href='/faq' passHref>
+            <p>FAQ</p>
+          </Link>
+        </div>
+        <div className='flex flex-col gap-2'>
+          <h3 className='font-bold text-hima-dark dark:text-hima-white'>Legal</h3>
           <Link href='/privacy' passHref>
             <p>Privacy Policy</p>
           </Link>
@@ -36,16 +49,10 @@ const RightSection = () => {
             <p>Terms of Service</p>
           </Link>
         </div>
-        <div className='flex flex-col gap-3'>
-          <h3 className='font-bold text-hima-dark dark:text-hima-white'>Product</h3>
-          <Link href='/subscription' passHref>
-            <p>Pricing</p>
-          </Link>
-        </div>
-        <div className='flex flex-col gap-3'>
-          <h3 className='font-bold text-hima-dark dark:text-hima-white'>Support</h3>
-          <Link href='mailto:violet@himari.sg' passHref>
-            <p>Email</p>
+        <div className='flex flex-col gap-2'>
+          <h3 className='font-bold text-hima-dark dark:text-hima-white'>Contribute</h3>
+          <Link href='/upload' passHref>
+            <p>Upload</p>
           </Link>
         </div>
       </div>
@@ -54,18 +61,17 @@ const RightSection = () => {
 };
 
 export const Footer = () => {
+  const currentYear = new Date().getFullYear();
+
   return (
     <footer className='w-4/5 flex flex-col mx-auto items-center mb-16 mt-16'>
       <div className='flex flex-col md:flex-row gap-16 md:gap-0 w-full justify-between items-start'>
         <LeftSection />
         <RightSection />
       </div>
-      <div className='w-full'>
+      <div className='w-full mt-3'>
         <Divider className='w-full border-t-2 border-t-gray-600 mt-4 mb-2' />
-        <p>
-          © 2025 Hima Boilerplate - All rights reserved. • Questions or business enquiries? Send an
-          email to violet@himari.sg
-        </p>
+        <p>© 2023 - {currentYear} Holy Grail Team • Questions? Contact us at grail@himaa.me</p>
       </div>
     </footer>
   );
