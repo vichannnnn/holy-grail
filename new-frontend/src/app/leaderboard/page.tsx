1d0ca304f2a9915b56b59e2f67a4c8751094d636
Eugene <violet@himaa.me>
2025-03-24 04:00:07 +0900

    feat: add leaderboard component

---
+++ new-frontend/src/app/leaderboard/page.tsx
@@ -0,0 +1,11 @@
+import { Leaderboard } from '@layouts/Leaderboard';
+
+const LeaderboardPage = () => {
+  return (
+    <>
+      <Leaderboard />
+    </>
+  );
+};
+
+export default LeaderboardPage;
