768bc523556ada8a30ffa8a4b0fcf34695d72356
Eugene <violet@himaa.me>
2025-08-08 16:58:13 +0900

    chore: update ads

---
+++ holy-grail-backend/backend/app/services/__init__.py
@@ -0,0 +1,5 @@
+from .analytics_service import AnalyticsService
+from .auth_service import AuthService
+from .library_service import LibraryService
+
+__all__ = ["AuthService", "LibraryService", "AnalyticsService"]
--- holy-grail-frontend/src/components/ui/Showcase/Showcase.tsx
+++ holy-grail-frontend/src/components/ui/Showcase/Showcase.tsx
@@ -9,7 +9,7 @@ import { adClick, adImpression } from '@api/analytics';
 
 import { MediaQueryContext } from '@providers/MediaQueryProvider';
 
-const ADS_IMAGE_URL = 'https://image.himaa.me/TURIS_VPN_4.png';
+const ADS_IMAGE_URL = 'https://image.himaa.me/TURIS_VPN_5.jpg';
 
 interface InfoButtonProps {
   isMobile: boolean;
