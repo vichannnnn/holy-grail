fba508b5106bfb12c6c00aebb1e9eb11e53440d6
Eugene <violet@himaa.me>
2025-08-30 17:11:52 +0900

    chore: update ads

--- holy-grail-frontend/src/components/ui/Showcase/Showcase.tsx
+++ holy-grail-frontend/src/components/ui/Showcase/Showcase.tsx
@@ -9,7 +9,7 @@ import { adClick, adImpression } from '@api/analytics';
 
 import { MediaQueryContext } from '@providers/MediaQueryProvider';
 
-const ADS_IMAGE_URL = 'https://image.himaa.me/TURIS_VPN_5.jpg';
+const ADS_IMAGE_URL = 'https://image.himaa.me/TURIS_VPN_6.jpg';
 
 interface InfoButtonProps {
   isMobile: boolean;
