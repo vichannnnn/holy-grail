e28e95a20d6d0476fc8e55572f27edd4370bbbbc
SebassNoob <sebastian.ong@hotmail.com>
2025-09-01 01:02:44 +0800

    fix: frontend dev server crashes

---
+++ apps/frontend/next.config.ts
@@ -0,0 +1,21 @@
+import type { NextConfig } from 'next';
+import { join } from 'path';
+
+const nextConfig: NextConfig = {
+  output: 'standalone',
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'image.himaa.me',
+        pathname: '**',
+      },
+    ],
+  },
+};
+
+if (process.env.NODE_ENV === 'development') {
+  nextConfig.outputFileTracingRoot = join(__dirname, '../../')
+}
+
+export default nextConfig;
