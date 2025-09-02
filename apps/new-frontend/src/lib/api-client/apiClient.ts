c8e195fa9bbd7f0a606bedbf591b84035189da95
SebassNoob <sebastian.ong@hotmail.com>
2025-09-02 21:24:47 +0800

    feat: WIP server side auth

---
+++ apps/new-frontend/src/lib/api-client/apiClient.ts
@@ -0,0 +1,38 @@
+import axios from "axios";
+
+const NEXT_PUBLIC_API_URL = process.env.NEXT_PUBLIC_API_URL;
+
+export const apiClient = axios.create({
+	baseURL: NEXT_PUBLIC_API_URL,
+	headers: {
+		"Content-Type": "application/json",
+	},
+});
+/*
+apiClient.interceptors.request.use(
+	(config) => {
+		const accessToken = localStorage.getItem("access_token");
+		if (accessToken) {
+			config.headers.Authorization = `Bearer ${accessToken}`;
+		} else {
+			localStorage.removeItem("user");
+			localStorage.removeItem("access_token");
+		}
+		return config;
+	},
+	(error) => {
+		return Promise.reject(error);
+	},
+);
+
+apiClient.interceptors.response.use(
+	(response) => response,
+	(error) => {
+		if (error.response && error.response.status === 401) {
+			localStorage.removeItem("user");
+			localStorage.removeItem("access_token");
+		}
+		return Promise.reject(error);
+	},
+);
+*/
---
+++ apps/new-frontend/src/lib/auth/getUser.tsx
@@ -0,0 +1,3 @@
+import "server-only";
+import { cookies } from "next/headers";
+import { apiClient } from "@lib/api-client";
