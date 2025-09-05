957f287fa95d352c6dc097c14116cd3c0a79f63c
SebassNoob <sebastian.ong@hotmail.com>
2025-09-05 22:56:41 +0800

    feat: WIP hook up library to api

--- apps/new-frontend/src/app/library/_components/LibraryContent/LibraryContent.tsx
+++ apps/new-frontend/src/app/library/_components/LibraryContent/LibraryContent.tsx
@@ -1,7 +1,21 @@
 "use client";
 import { Pagination } from "@lib/features/client";
+import type { LibraryAPIResponse, PaginatedNotes } from "../../types";
+import Image from "next/image";
+import { Title, Text } from "@shared/ui/components";
 
-export function LibraryContent() {
+export function LibraryContent({ ok, data, err }: LibraryAPIResponse<PaginatedNotes>) {
+	if (!ok) {
+		return (
+			<div className="flex flex-col items-center justify-center h-64">
+				<Image src="/trimmy-grail-chan-sparkling.webp" alt="Error" width={100} height={100} />
+				<Title order={2} className="font-bold mb-4">
+					We ran into an issue :(
+				</Title>
+				<Text>{err ?? "An unknown error occurred."}</Text>
+			</div>
+		);
+	}
 	return (
 		<Pagination currentPage={1} totalPages={10} onPageChange={(newPage) => console.log(newPage)} />
 	);
--- apps/new-frontend/src/app/library/page.tsx
+++ apps/new-frontend/src/app/library/page.tsx
@@ -2,43 +2,51 @@ import { getUser } from "@lib/auth";
 import Link from "next/link";
 import { Title, Text, Divider } from "@shared/ui/components";
 import { LibraryContent } from "./_components/LibraryContent/LibraryContent";
+import type { NotesSearchParams } from "./types";
+import { fetchApprovedNotes } from "./actions";
 
 export default async function LibraryPage({
 	searchParams,
 }: {
-	searchParams: Promise<{ [key: string]: string | string[] | undefined }>;
+	searchParams: Promise<NotesSearchParams>;
 }) {
-	console.log(await searchParams);
+	const query = await searchParams;
+	const response = await fetchApprovedNotes(query);
+
 	const user = await getUser();
 	return (
-		<main className="flex flex-col gap-2 mx-6 md:mx-12 my-4">
-			<Title order={1} className="font-bold text-3xl">
-				Hello
-				{user ? ` ${user.username}, welcome back to the Holy Grail` : ", welcome to the Holy Grail"}
-			</Title>
-			<Title order={2} className="font-normal text-lg">
-				A central repository for your revision materials, consolidated into a library.
-			</Title>
-			<Divider className="mx-1" />
-			<Title order={2} className="font-bold">
-				Library
-			</Title>
-			<Text>
-				View materials or contribute{" "}
-				<Link href="/upload" className="font-bold">
-					here
-				</Link>{" "}
-				after you have logged in (subjected to approval of administrators).
-			</Text>
-			<div className="flex flex-col">
-				<Text>Are you a tuition centre or freelance tutor looking to expand your reach?</Text>
-				<Text>We have one of the largest student-focused audiences in Singapore.</Text>
+		<main className="flex flex-col gap-12">
+			<div className="flex flex-col gap-2 mx-6 md:mx-12 my-4">
+				<Title order={1} className="font-bold text-3xl">
+					Hello
+					{user
+						? ` ${user.username}, welcome back to the Holy Grail`
+						: ", welcome to the Holy Grail"}
+				</Title>
+				<Title order={2} className="font-normal text-lg">
+					A central repository for your revision materials, consolidated into a library.
+				</Title>
+				<Divider className="mx-1" />
+				<Title order={2} className="font-bold">
+					Library
+				</Title>
 				<Text>
-					Interested sponsors and advertisers, please contact us at{" "}
-					<span className="font-bold">grail@himaa.me</span> to explore partnership opportunities.
+					View materials or contribute{" "}
+					<Link href="/upload" className="font-bold">
+						here
+					</Link>{" "}
+					after you have logged in (subjected to approval of administrators).
 				</Text>
+				<div className="flex flex-col">
+					<Text>Are you a tuition centre or freelance tutor looking to expand your reach?</Text>
+					<Text>We have one of the largest student-focused audiences in Singapore.</Text>
+					<Text>
+						Interested sponsors and advertisers, please contact us at{" "}
+						<span className="font-bold">grail@himaa.me</span> to explore partnership opportunities.
+					</Text>
+				</div>
 			</div>
-			<LibraryContent />
+			<LibraryContent {...response} />
 		</main>
 	);
 }
