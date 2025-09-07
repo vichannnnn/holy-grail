e0cf7b2132262f0730f3795912fc43d160add2a9
SebassNoob <sebastian.ong@hotmail.com>
2025-09-08 02:47:48 +0800

    feat: hook up analytics

--- apps/new-frontend/src/app/_components/Analytics/Analytics.tsx
+++ apps/new-frontend/src/app/_components/Analytics/Analytics.tsx
@@ -1,33 +1,16 @@
 import { StatCard } from "./StatCard";
+import { fetchAnalytics } from "./actions";
 
-// import { AnalyticsResponse, getAnalytics } from "@api/analytics";
-
-const analyticsData = {
-	file_download_count: 69420,
-	unique_active_users: 999,
-	user_count: 67,
-};
-
-export const Analytics = () => {
-	/*
-	const [analyticsData, setAnalyticsData] = useState<AnalyticsResponse>(defaultAnalyticsData);
-
-	useEffect(() => {
-		const fetchAnalytics = async () => {
-			const data = await getAnalytics();
-			setAnalyticsData(data);
-		};
-		fetchAnalytics().then((r) => null);
-	}, []);
-  */
+export async function Analytics() {
+	const analyticsData = await fetchAnalytics();
 
 	return (
 		<div className="w-4/5 md:w-3/5 mx-auto mt-8">
 			<div className="grid grid-cols-1 md:grid-cols-3">
-				<StatCard title="Notes Downloaded" value={analyticsData.file_download_count} />
-				<StatCard title="Students Visited" value={analyticsData.unique_active_users} />
-				<StatCard title="Accounts Created" value={analyticsData.user_count} />
+				<StatCard title="Notes Downloaded" value={analyticsData?.file_download_count ?? 0} />
+				<StatCard title="Students Visited" value={analyticsData?.unique_active_users ?? 0} />
+				<StatCard title="Accounts Created" value={analyticsData?.user_count ?? 0} />
 			</div>
 		</div>
 	);
-};
+}
