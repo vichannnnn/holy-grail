5c673a75dbf1fb5cbc52dc8887dacf08a894109d
SebassNoob <sebastian.ong@hotmail.com>
2025-09-08 02:47:30 +0800

    fix: minor nitpicks

--- packages/ui/src/components/Card/Card.tsx
+++ packages/ui/src/components/Card/Card.tsx
@@ -2,7 +2,7 @@ import type { CardProps } from "./types";
 import { twMerge } from "tailwind-merge";
 
 const defaultStyles =
-	"flex flex-col max-w-lg bg-white border border-gray-200 rounded-md shadow dark:shadow-none dark:bg-zinc-950 dark:border-zinc-900";
+	"flex flex-col max-w-lg bg-white border border-gray-200 rounded-md shadow dark:shadow-none dark:bg-zinc-900 dark:border-0";
 
 export function Card({ children, className, ...rest }: CardProps) {
 	const mergedStyles = twMerge(defaultStyles, className);
