9fa50ee76c8831c185930f9707cd893144b0b313
SebassNoob <sebastian.ong@hotmail.com>
2025-09-08 02:47:48 +0800

    feat: hook up analytics

--- apps/new-frontend/src/app/_components/Analytics/types.ts
+++ apps/new-frontend/src/app/_components/Analytics/types.ts
@@ -2,3 +2,9 @@ export interface StatCardProps {
 	title: string;
 	value: number;
 }
+
+export interface AnalyticsResponse {
+	file_download_count: number;
+	unique_active_users: number;
+	user_count: number;
+}
