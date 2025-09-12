1b4f99aa8fe7f9c194a790e4bc2d67ab9071a347
SebassNoob <sebastian.ong@hotmail.com>
2025-09-13 00:40:55 +0800

    fix: fatal nav error, hardcoded application/json header

--- apps/new-frontend/src/app/auth/register/RegisterForm.tsx
+++ apps/new-frontend/src/app/auth/register/RegisterForm.tsx
@@ -8,9 +8,10 @@ import { useTransition } from "react";
 import { PasswordInput } from "../_components";
 import { register as registerUser } from "./actions";
 import toast from "react-hot-toast";
-import { redirect } from "next/navigation";
+import { useRouter } from "next/navigation";
 
 export function RegisterForm() {
+  const router = useRouter();
 	const {
 		register,
 		handleSubmit,
@@ -27,7 +28,7 @@ export function RegisterForm() {
 				toast.error(message || "Registering failed");
 			} else {
 				toast.success("Registering successful! Redirecting...");
-				redirect("/library");
+				router.push("/library");
 			}
 		});
 	};
