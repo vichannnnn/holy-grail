fd2c6869243879bc7c11445565b1faaeeedc383d
SebassNoob <sebastian.ong@hotmail.com>
2025-09-13 23:26:16 +0800

    feat: add binding for forgot password

--- apps/new-frontend/src/app/auth/forgot-password/schemas.ts
+++ apps/new-frontend/src/app/auth/forgot-password/schemas.ts
@@ -3,3 +3,5 @@ import { z } from "zod";
 export const ForgotPasswordSchema = z.object({
 	email: z.email("Invalid email address"),
 });
+
+export type ForgotPasswordFormData = z.infer<typeof ForgotPasswordSchema>;
