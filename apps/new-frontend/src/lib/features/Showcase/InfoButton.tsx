ee000980d942b8858ce09c9838690366fc1153ba
SebassNoob <sebastian.ong@hotmail.com>
2025-09-15 22:18:01 +0800

    chore: add ad

---
+++ apps/new-frontend/src/lib/features/Showcase/InfoButton.tsx
@@ -0,0 +1,34 @@
+"use client";
+
+import { Popover, PopoverButton, PopoverPanel } from "@headlessui/react";
+import { IconButton } from "@shared/ui/components";
+import { InformationCircleIcon } from "@heroicons/react/24/outline";
+
+export function InfoButton() {
+	return (
+		<Popover className="absolute -top-1 -left-1 md:top-0.5 md:left-0">
+			<PopoverButton
+				as={IconButton}
+				size="sm"
+				className="bg-black/20 text-white hover:bg-black/30"
+				aria-label="Why am I seeing this ad?"
+			>
+				<InformationCircleIcon className="size-4" />
+			</PopoverButton>
+
+			<PopoverPanel className="absolute z-10 w-80 px-4 mt-3 transform -translate-x-1/2 left-1/2 sm:px-0">
+				<div className="overflow-hidden rounded-lg shadow-lg">
+					<div className="relative bg-white p-6 dark:bg-gray-800">
+						<div className="text-base font-medium text-gray-900 dark:text-white">
+							Why am I seeing this ad?
+						</div>
+						<div className="mt-2 text-sm text-gray-600 dark:text-gray-300">
+							This is an advertisement from our sponsor to help with our hosting and infrastructure
+							cost in keeping this project alive.
+						</div>
+					</div>
+				</div>
+			</PopoverPanel>
+		</Popover>
+	);
+}
---
+++ apps/new-frontend/src/lib/features/Showcase/constants.ts
@@ -0,0 +1,4 @@
+export const ADS_IMAGE_URL = "https://image.himaa.me/TURIS_VPN_6.png";
+export const ADS_ALT_TEXT = "Turis VPN";
+export const ADS_REDIRECT_URL =
+	"https://play.google.com/store/apps/details?id=com.turisvpn.tech&pcampaignid=web_share";
