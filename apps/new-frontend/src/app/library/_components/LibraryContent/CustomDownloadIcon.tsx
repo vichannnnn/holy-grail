e024e6d433b0aaa788f0fe9f92d8529376417536
SebassNoob <sebastian.ong@hotmail.com>
2025-09-21 14:52:21 +0800

    chore: revert new download icon

---
+++ apps/new-frontend/src/app/library/_components/LibraryContent/CustomDownloadIcon.tsx
@@ -0,0 +1,9 @@
+import type { SVGProps } from "react";
+
+export function CustomDownloadIcon(props: SVGProps<SVGSVGElement>) {
+	return (
+		<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" strokeWidth={1} {...props}>
+			<path d="M5 20h14v-2H5zM19 9h-4V3H9v6H5l7 7z" />
+		</svg>
+	);
+}
