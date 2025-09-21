cad42b6f8fcb5907ff1c224563b1c5f0d2d7ec42
SebassNoob <sebastian.ong@hotmail.com>
2025-09-21 21:22:55 +0800

    fix: improve header accessibility

--- apps/new-frontend/src/lib/features/Header/_components/HeaderRight/DropdownButton.tsx
+++ apps/new-frontend/src/lib/features/Header/_components/HeaderRight/DropdownButton.tsx
@@ -3,6 +3,7 @@ import type { DropdownButtonProps } from "./types";
 export function DropdownButton({ label, icon, ...rest }: DropdownButtonProps) {
 	return (
 		<button
+			aria-label="Dropdown Button"
 			className="block w-full px-2 py-1 rounded-sm hover:bg-gray-200 dark:hover:bg-zinc-600 text-left cursor-pointer"
 			tabIndex={0}
 			{...rest}
