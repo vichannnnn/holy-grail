6b515778aaadaa6d0da578ca17e7dea0cb9018fe
SebassNoob <sebastian.ong@hotmail.com>
2025-11-02 19:13:37 +0800

    feat: add skeleton component

---
+++ packages/ui/src/components/Skeleton/Skeleton.tsx
@@ -0,0 +1,11 @@
+import type { SkeletonProps } from "./types";
+import { twMerge } from "tailwind-merge";
+
+const defaultStyles =
+	"animate-pulse bg-gray-200 rounded dark:bg-zinc-800";
+
+export function Skeleton({ className, ...rest }: SkeletonProps) {
+	const mergedStyles = twMerge(defaultStyles, className);
+
+	return <div className={mergedStyles} {...rest} />;
+}
